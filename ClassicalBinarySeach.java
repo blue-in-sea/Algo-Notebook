394a5ac27eec6bb6e3f06105b2d68e44bc3aac10
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-10 21:34:20 -0600

    Create ClassicalBinarySeach.java

---
+++ ClassicalBinarySeach.java
@@ -0,0 +1,24 @@
+/*
+ * assume no duplicate in the input array
+ */
+
+public class ClassicalBinarySearch {
+  public int binarySearch(int[] array, int target) {
+    if (array == null || array.length == 0) {
+      return -1;
+    }
+    int left = 0;
+    int right = array.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (array[mid] == target) {
+        return mid;
+      } else if (array[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
