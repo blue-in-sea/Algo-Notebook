a68c384bdd3877d0e621690c1071bb148ee2d7f5
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-10 21:10:09 -0600

    Create Partition.java

---
+++ Partition.java
@@ -0,0 +1,60 @@
+
+import java.util.Arrays;
+
+public class Partition {
+    public void partition(int[] array, int pivotIndex) {
+        // corner case
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        // partition
+        int pivot = array[pivotIndex];
+        // swap the pivot element to the rightmost position first
+        int lastIndex = array.length - 1;
+        swap(array, pivotIndex, lastIndex);
+
+        int left = 0;
+        int right = lastIndex - 1;
+        while (left <= right) {
+            if (array[left] <= pivot) {
+                left++;
+            } else if (array[right] >= pivot) {
+                right--;
+            } else {
+                swap(array, left++, right--);
+            }
+        }
+        // swap back the pivot element
+        swap(array, left, lastIndex);
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int tmp = array[left];
+        array[left] = array[right];
+        array[right] = tmp;
+    }
+
+    public static void main(String[] args) {
+        Partition solution = new Partition();
+
+        int[] array = null;
+        solution.partition(array,0);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[0];
+        solution.partition(array,0);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[] {3, 2, 1};
+        solution.partition(array,0);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[] {3, 2, 1, 2};
+        solution.partition(array, 1);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[] {4, 8, 5, 6, 3};
+        solution.partition(array, 2);
+        System.out.println(Arrays.toString(array));
+    }
+}
