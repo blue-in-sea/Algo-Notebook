4e0988fb59aac2f5fdf29ffc108b7eee2196dd6e
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-11 18:30:30 -0600

    Create ZigZagGetKeyInBinaryTree.java

---
+++ ZigZagGetKeyInBinaryTree.java
@@ -0,0 +1,12 @@
+/* *
+ * Get the list of keys in a given binary tree layer by layer in zig-zag order.
+ * 
+ * Examples
+ *         5
+ *       /   \
+ *     3      8
+ *   /   \      \
+ *  1     4       11
+ *
+ * the result is [5, 3, 8, 11, 4, 1]
+ */
