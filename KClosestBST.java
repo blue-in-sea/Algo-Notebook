4c6a4060a35314c5379a5bf46ce2918e55764403
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-12 01:22:02 -0600

    Update KClosestBST.java

--- KClosestBST.java
+++ KClosestBST.java
@@ -4,36 +4,36 @@ import java.util.List;
 import java.util.Deque;
 
 class TreeNode {
-  int key;
-  TreeNode left;
-  TreeNode right;
-  
-  TreeNode(int key) {
-    this.key = key;
-  }
+    int key;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int key) {
+        this.key = key;
+    }
 }
 
 public class KClosestBST {
-  public List<Integer> closestKValues(TreeNode root, double target, int k) {
-    Deque<Integer> dq = new ArrayDeque<>();    
-    inOrder(root, dq);
-    
-    while (dq.size() > k) {
-      if (Math.abs(dq.peekFirst() - target) > Math.abs(dq.peekLast() - target)) {
-        dq.pollFirst();
-      } else {
-        dq.pollLast();
-      }
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        inOrder(root, dq);
+
+        while (dq.size() > k) {
+            if (Math.abs(dq.peekFirst() - target) > Math.abs(dq.peekLast() - target)) {
+                dq.pollFirst();
+            } else {
+                dq.pollLast();
+            }
+        }
+
+        return new ArrayList<>(dq);
     }
-    
-    return new ArrayList<>(dq);
-  }
 
-  private void inOrder(TreeNode root, Deque<Integer> dq) {
-    if (root == null) return;
+    private void inOrder(TreeNode root, Deque<Integer> dq) {
+        if (root == null) return;
 
-    inOrder(root.left, dq);
-    dq.add(root.key);
-    inOrder(root.right, dq);
-  }
+        inOrder(root.left, dq);
+        dq.add(root.key);
+        inOrder(root.right, dq);
+    }
 }
