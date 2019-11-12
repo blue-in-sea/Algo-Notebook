856fe7489a0dd5cb312dce5da54c5debab87e9e7
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-11 21:40:55 -0600

    Update IsFullBinaryTree.java

--- IsFullBinaryTree.java
+++ IsFullBinaryTree.java
@@ -1,22 +1,12 @@
 /* *
- * Determine if a given binary tree is full.
+ * Determine if a given binary tree is full (If the root is null, return false).
+ *
  * A full binary tree is defined as a binary tree in which all nodes have either zero or two child nodes. 
  * Conversely, there is no node in a full binary tree, which has one child node.
- * If the root is null, return false.
  *
  * Recursion: 1) Termination condition: 终止条件
- *            2) Return value: 返回值
- * [base case] if it is a leaf node: return true
- * [recursive rules]
- * 1. What should we do at current layer? 
- *    if the node has both left and right child: 
- *        recursively check its left and right subtree  
- *    else 
- *        return false
- * 2. What we expect from our left and right child?
- *    valid full tree or invalid?
- * 3. What should we return to our parent layer?
- *    (same as processure 1)     
+ *            2) Recursive calls: 调用单元做了什么
+ *            3) Return value: 返回值
  */
 
 public class TreeNode {
@@ -30,6 +20,23 @@ public class TreeNode {
 }
 
 public class isFullBinaryTree {
+  /**
+   * Method: Recursion
+   *
+   * base case: if it is a leaf node: return true
+   *
+   * 1. What should we do at current layer? 
+   *    if the node has both left and right child: 
+   *        recursively check its left and right subtree  
+   *    else 
+   *        return false
+   *
+   * 2. What we expect from our left and right child?
+   *    (valid full tree or invalid?)
+   *
+   * 3. What should we return to our parent layer?
+   *    (same as processure 1)  
+   */
   public boolean isFull(TreeNode root) {
     // corner case
     if (root == null) {
