2130a505ec3d7dfc9c8072be04577b202ec9ba41
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-12 02:18:03 -0600

    Update LargestNumberSmallerInBinarySearchTree.java

--- LargestNumberSmallerInBinarySearchTree.java
+++ LargestNumberSmallerInBinarySearchTree.java
@@ -1,5 +1,16 @@
-public class TreeNode {
+/* *
+ * In a binary search tree, find the node containing the largest number smaller than the given target number.
+ * If there is no such number, return Integer.MIN_VALUE.
+ */
 
+public class TreeNode {
+  int key;
+  TreeNode left;
+  TreeNode right;
+  
+  TreeNode(int key) {
+    this.key = key;
+  }
 }
 
 public class LargestNumberSmallerInBinarySearchTree {
