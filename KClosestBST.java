d6eb3858079f6971767d987244be22e9c97cd002
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-12 01:30:27 -0600

    Update KClosestBST.java

--- KClosestBST.java
+++ KClosestBST.java
@@ -1,3 +1,10 @@
+/* *
+ * In a binary search tree, find k nodes containing the closest numbers to the given target number. 
+ * return them in sorted array (ascending)
+ *
+ * Assmumption: root is not null, and no duplicate keys in the given binary search tree.
+ */
+
 public class TreeNode {
   int key;
   TreeNode left;
