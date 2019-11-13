63512bb01598a19d7102f9078afc7c9556e9503a
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-13 16:24:54 -0600

    Update IsValidBinarySearchTree.java

--- IsValidBinarySearchTree.java
+++ IsValidBinarySearchTree.java
@@ -1,4 +1,27 @@
+public class TreeNode {
+  int key;
+  TreeNode left;
+  TreeNode right;
+  
+  TreeNode(int key) {
+    this.key = key;
+  }
+}
+
 public class IsValidBinarySearchTree {
-
-
+  // Time: O(N)
+  // Space: O(H)
+  public boolean isBST(TreeNode root) {
+    return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+  
+  private boolean checkBST(TreeNode root, int min, int max) {
+    if (root == null) {
+      return true;
+    }
+    if (root.key < min || root.key > max) {
+      return false;
+    }
+    return checkBST(root.left, min, root.key - 1) && checkBST(root.right, root.key + 1, max);
+  }
 }
