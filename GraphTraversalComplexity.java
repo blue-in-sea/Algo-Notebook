9d8ffbe3ac5e9b5d3cf9015dc040256dbc96e27b
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-12 18:15:37 -0600

    Update GraphTraversalComplexity.java

--- GraphTraversalComplexity.java
+++ GraphTraversalComplexity.java
@@ -3,6 +3,51 @@
  * Traverse all nodes in Graph<ListNode> graph (adjacency list).
  */
 
+class GraphNode {
+  public int value;
+  public List<GraphNode> nei;
+  boolean visited;
+  
+  public GraphNode(int value) {
+    this.value = value;
+    nei = new ArrayList<>();
+    visited = false;
+  }
+}
+
 public class GraphTraversalComplexity {
+  /* *
+   * DFS Traversal: Complexity Analysis 
+   * Time: O(V * (1 + E/V)) = O(V + E)
+   * Spcae: O(V) 
+   */
+  public void traverse(Graph<ListNode> graph) {
+    for (GraphNode n : graph) {
+      if (!n.visited) {
+        dfs(n);
+      }
+    }
+  }
+  
+  /* *
+   * V := Number of Nodes 
+   * E := Number of Edges 
+   */
+  priavte dfs(GraphNode node) { // O(V)
+    node.visited = true; // O(1)
+    for (GraphNode n : node.nei) { // O(E/V)
+      if (!n.visited) {
+        dfs(n);
+      }
+    }
+  }
 
 }
+
+/* *
+ * Given V: # of vertices on the graph 
+ * E min -> 0
+ * E     -> O(V)
+ * E max -> O(V^2)
+ */
+
