1ba85bbe0736eaf8326799f08d3c7b576353edef
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-12 22:12:51 -0600

    Create CheckIfBinaryTreeIsBalanced.java

---
+++ CheckIfBinaryTreeIsBalanced.java
@@ -0,0 +1,41 @@
+public class TreeNode {
+  int key;
+  TreeNode left;
+  TreeNode right;
+  
+  TreeNode(int key) {
+    this.key = key;
+  }
+}
+
+public class CheckIfBinaryTreeIsBalanced {
+  // Method 1: use depth() only to indicate
+  // Time: O(N)
+  // Spce: O(H)
+  public boolean isBalanced(TreeNode root) {
+    if (root == null)  return true;  // corner case
+    return depth(root) != -1;
+  }
+  // 从顶至底DFS，以每个节点为根节点，递归判断是否是平衡二叉树
+  // 当我们发现有一例 左/右子树高度差 ＞ 1 的情况时，代表此树不是平衡树，返回-1；
+  private int depth(TreeNode root) { 
+    if (root == null) return 0;
+    int left = depth(root.left);
+    int right = depth(root.right);
+    if (left == -1 || right == -1)  return -1;
+    return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+  }
+  
+  // Method 2: 从顶至底DFS，以每个节点为根节点，递归判断是否是平衡二叉树
+  // Time: O(N^2)
+  // Spce: O(H)
+  public boolean isBalanced(TreeNode root) {
+    if (root == null)  return true;
+    return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+  }
+  
+  private int depth(TreeNode root) { 
+    if (root == null) return 0;
+    return Math.max(depth(root.left), depth(root.right)) + 1;
+  }
+}
