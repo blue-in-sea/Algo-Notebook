e3fc03f9d1fe9b6a44d7a7c17d3487f92daa4482
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-14 17:07:41 -0600

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -2,18 +2,18 @@ public class MergeSort {
   public int[] mergeSort(int[] array) {
     // corner case 
     if (array == null || array.length <= 1) {
-      return array
+      return array;
     }
     // allocate a helperArray to help with merge step
     // so that we guarantee no more than O(n) space is used
     // the spece complexity in this case is O(n) 
-    int[] helper = new int[array.length - 1];
+    int[] helper = new int[array.length];
     mergeSortR(array, helper, 0, array.length - 1);
     return array;
   }
   
-  priavte void mergeSortR(int[] array, int[] helper, int left, int right) {
-    while (left > right) {
+  private void mergeSortR(int[] array, int[] helper, int left, int right) {
+    while (left >= right) {
       return;
     }
     int mid = left + (right - left) / 2;
@@ -22,22 +22,22 @@ public class MergeSort {
     merge(array, helper, left, mid, right);
   }
   
-  private merge(int[] array, int[] helper, int left, int mid, int right) {
+  private void merge(int[] array, int[] helper, int left, int mid, int right) {
     for (int i = 0; i < array.length; i++) {
       helper[i] = array[i];
     }
     int i = left;     // left index
     int j = mid + 1;  // right index
-    while (i <= mid && j < right) {
-      if (helper[i] < helper[j]) {
-        array[i++] = helper[i++];
+    while (i <= mid && j <= right) {
+      if (helper[i] <= helper[j]) {
+        array[left++] = helper[i++];
       } else {
-        array[i++] = helper[j++];
+        array[left++] = helper[j++];
       }
     }
     // if we have some elements at left side, we need copy them 
     while (i <= mid) {
-      array[i++] = helper[i++];
+      array[left++] = helper[i++];
     }
     // if we still have some elements at right side, no need to copy
     // because they already in their position 
