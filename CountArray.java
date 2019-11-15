dd7d3d572ac156faade1de301a91dd342b208b3e
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-14 18:44:35 -0600

    Update CountArray.java

--- CountArray.java
+++ CountArray.java
@@ -1,3 +1,15 @@
+/* *
+ * Given an array A of length N containing all positive integers from [1...N]. 
+ * How to get an array B such that B[i] represents: 
+ * how many elements A[j] (j > i) in array A that are smaller than A[i].
+ *
+ * Assume the given array A is not null.
+ * A = { 4, 1, 3, 2 }, we should get B = { 3, 0, 1, 0 }.
+ *
+ * Time Complexity: O(nlogn)
+ * Space Complexity: O(3n) --> O(n)
+ */
+
 public class CountArray {
   private int[] indexArray;
   private int[] countArray;  // the actual return array
