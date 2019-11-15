8943cc4bb5eabae3f4dd286109624baff7f69bba
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-14 23:48:29 -0600

    Update ClassicalBinarySeach.java

--- ClassicalBinarySeach.java
+++ ClassicalBinarySeach.java
@@ -1,13 +1,52 @@
-/* *
+/**
  * Given a target integer T and an integer array A sorted in ascending order, 
  * find the index i such that A[i] == T or return -1 if there is no such index. 
- * Assumption: no duplicate elements in the array. 
+ *
+ * Assumption: There can be duplicate elements in the array, 
+ * and you can return any of the indices i such that A[i] == T.
  */
 
 public class ClassicalBinarySearch {
-  // Time: O(logn)
-  // Space: O(1)
-  public int binarySearch(int[] array, int target) {
+  /**
+   * @param nums: The integer array.
+   * @param target: Target to find.
+   * @return: The first position of target. Position starts from 0.
+   */
+  pulic int binarySearch(int[] array, int target) {
+    if (array == null || array.length == 0) {
+      return -1;
+    }
+    int left = 0;
+    int right = array.length - 1;
+    while (left + 1 < right) {
+      int mid = left + (right - left) / 2;
+      if (array[mid] == target) {
+        return mid;
+      } else if (array[mid] < target) {
+        left = mid;
+        // or left = mid + 1;
+      } else {
+        right = mid;
+        // or right = mid - 1;
+      }
+    }
+    
+    if (array[left] == target) {
+      return left;
+    }
+    if (array[right] == target) {
+      return right;
+    } 
+    return -1;    
+    }
+}
+  
+  /**
+   * @param nums: The integer array.
+   * @param target: Target to find.
+   * @return: The first position of target. Position starts from 0.
+   */
+  pulic int binarySearch(int[] array, int target) {
     if (array == null || array.length == 0) {
       return -1;
     }
