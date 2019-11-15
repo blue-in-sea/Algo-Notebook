638bed081aef67c65f7e1151c9c65f62d1387dc1
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-14 18:41:19 -0600

    Update CountArray.java

--- CountArray.java
+++ CountArray.java
@@ -1,3 +1,16 @@
 public class CountArray {
-
+  private int[] indexArray;
+  private int[] countArray;  // the actual return array
+  private int[] helper;      // helpe with merge sorting indices 
+  
+  public int[] countArray(int[] array) { 
+    // assume the array is not null;
+    indexArray = initialindexArray(array);
+    countArray = new int[array.length];
+    helper = new int[array.length];
+    mergeSort();
+    mergeSort();
+    merge();
+    return countArray;
+  }
 }
