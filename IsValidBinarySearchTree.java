39dd62a6636b691730c0c883e6ccaecc567799d1
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-15 00:10:24 -0600

    Update IsValidBinarySearchTree.java

--- IsValidBinarySearchTree.java
+++ IsValidBinarySearchTree.java
@@ -1,6 +1,5 @@
 /**
- * Determine if a given binary tree is binary search tree.There should no be duplicate keys in binary search tree.
- *
+ * Determine if a given binary tree is binary search tree. There should no be duplicate keys in binary search tree.
  * Assumptions: the keys stored in the binary search tree can not be Integer.MIN_VALUE or Integer.MAX_VALUE.
  * Corner Cases: What if the binary tree is null? Return true in this case.
  */
