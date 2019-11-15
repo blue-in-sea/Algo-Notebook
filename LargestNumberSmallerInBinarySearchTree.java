14060ee055e09487e56b625ea173eda328089dee
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-15 00:22:40 -0600

    Update LargestNumberSmallerInBinarySearchTree.java

--- LargestNumberSmallerInBinarySearchTree.java
+++ LargestNumberSmallerInBinarySearchTree.java
@@ -1,22 +1,24 @@
-/* *
+/**
  * In a binary search tree, find the node containing the largest number smaller than the given target number.
  * If there is no such number, return Integer.MIN_VALUE.
  */
 
-public class TreeNode {
-  int key;
-  TreeNode left;
-  TreeNode right;
-  
-  TreeNode(int key) {
-    this.key = key;
-  }
-}
-
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class LargestNumberSmallerInBinarySearchTree {
-  /* *
-   * Time: O(height) since cur either go left or go right along the tree 
-   * Space: O(1)     no extra space used 
+  /**
+   * Time: O(H) where average case O(logn), worst case O(n)
+   *       since cur either go left or go right along the tree 
+   * Space: O(1) no extra space used 
    */
   public int largestSmaller(TreeNode root, int target) {
     // assume tree is not null and no duplicate keys
