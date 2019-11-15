198050e3a201d1b26a6d30d213289e96dc2c8242
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-14 23:59:21 -0600

    Update CheckIfBinaryTreeIsBalanced.java

--- CheckIfBinaryTreeIsBalanced.java
+++ CheckIfBinaryTreeIsBalanced.java
@@ -9,7 +9,6 @@
  *     }
  * }
  */
-
 public class CheckIfBinaryTreeIsBalanced {
   // Method 1: use depth() only to indicate
   // Time: O(N)
@@ -24,8 +23,8 @@ public class CheckIfBinaryTreeIsBalanced {
     if (root == null) return 0;
     int left = depth(root.left);
     int right = depth(root.right);
-    if (left == -1 || right == -1)  return -1;
-    return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    if (left == -1 || right == -1 || Math.abs(left - right) > 1)  return -1;
+    return Math.max(left, right) + 1;
   }
   
   // Method 2: 从顶至底DFS，以每个节点为根节点，递归判断是否是平衡二叉树
