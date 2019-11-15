778a7924952bdcbad5476bf3669e5d04a611bf0b
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-14 18:58:01 -0600

    Update CountArray.java

--- CountArray.java
+++ CountArray.java
@@ -16,13 +16,76 @@ public class CountArray {
   private int[] helper;      // helpe with merge sorting indices 
   
   public int[] countArray(int[] array) { 
-    // assume the array is not null;
-    indexArray = initialindexArray(array);
+    indexArray = initialIndexArray(array);
     countArray = new int[array.length];
     helper = new int[array.length];
-    mergeSort();
-    mergeSort();
-    merge();
+   
+    mergeSort(array, 0, array.length - 1);
     return countArray;
+  }
+  
+  /* *
+   * 针对数组 array 指定的区间 [left, right] 进行归并排序，在排序的过程中完成统计任务
+   *
+   * @param array
+   * @param left
+   * @param right
+   */
+  private void mergeSort(int[] array, int left, int right) {
+    if (left >= right) {
+      return; // 数组只有一个元素的时候，没有比较，不统计
+    }
+    int mid = left + (right - left) / 2;
+    mergeSort(array, left, mid);
+    mergeSort(array, mid + 1, right);
+    
+    // 归并排序的优化，同样适用于该问题
+    // 如果索引数组有序，就没有必要再继续计算了
+    if (array[indexArray[mid]] > array[indexArray[mid + 1]]) {
+      merge(array, left, mid, right);
+    }
+  }
+  
+  /* *
+   * merge 谁小移动谁
+   * [left, mid] 是排好序的
+   * [mid + 1, right] 是排好序的
+   *
+   * @param array
+   * @param left
+   * @param mid
+   * @param right
+   */  
+  private void merge(int[] array, int left, int mid, int right) {
+    for (int i = left; i <= right; i++) {
+      helper[i] = indexArray[i];
+    }
+    
+    int i = left;
+    int j = mid + 1; 
+    int cur = left;
+    
+    while (i <= mid) {
+      if (j > right || array[helper[i]] <= array[helper[j]]) {
+        countArray[helper[i]] += (j - mid - 1); 
+        indexArray[cur++] = helper[i++];
+      } else {
+        // no need to update count
+        indexArray[cur++] = helper[j++];
+      }
+    }
+  }
+  
+  /* *
+   * IndexArray store the indices
+   * merge sort the array but records the sequence of 
+   * original indices after sorting
+   */
+  private int[] initialIndexArray(int[] array) {
+    int[] indices = new int[array.length];
+    for (int i = 0; i < array.length; i++) {
+      indices[i] = i;
+    }
+    return indices;
   }
 }
