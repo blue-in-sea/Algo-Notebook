c64ce3d90b29b25151d660bc8d35143b4df5d2d2
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-15 00:09:19 -0600

    Update IsValidBinarySearchTree.java

--- IsValidBinarySearchTree.java
+++ IsValidBinarySearchTree.java
@@ -1,26 +1,29 @@
-/* *
+/**
  * Determine if a given binary tree is binary search tree.There should no be duplicate keys in binary search tree.
  *
- * Assumptions
- * the keys stored in the binary search tree can not be Integer.MIN_VALUE or Integer.MAX_VALUE.
- *
- * Corner Cases
- * What if the binary tree is null? Return true in this case.
+ * Assumptions: the keys stored in the binary search tree can not be Integer.MIN_VALUE or Integer.MAX_VALUE.
+ * Corner Cases: What if the binary tree is null? Return true in this case.
  */
 
-public class TreeNode {
-  int key;
-  TreeNode left;
-  TreeNode right;
-  
-  TreeNode(int key) {
-    this.key = key;
-  }
-}
-
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int key;
+ *     public TreeNode left, right;
+ *     public TreeNode(int key) {
+ *         this.key = key;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class IsValidBinarySearchTree {
-  // Time: O(N)
-  // Space: O(H)
+  /**
+   * @param root: The root of binary tree.
+   * @return: True if the binary tree is BST, or false
+   *
+   * Time: O(N)
+   * Space: O(H)
+   */
   public boolean isBST(TreeNode root) {
     return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
   }
@@ -32,6 +35,7 @@ public class IsValidBinarySearchTree {
     if (root.key < min || root.key > max) {
       return false;
     }
-    return checkBST(root.left, min, root.key - 1) && checkBST(root.right, root.key + 1, max);
+    return checkBST(root.left, min, root.key - 1) 
+       && checkBST(root.right, root.key + 1, max);    // divide-and-conquer
   }
 }
