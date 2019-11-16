d10e2bd853a7662ce4c355c4ed1506c4e7afc068
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-16 13:15:35 -0600

    Update MaxProductOfCuttingRope.java

--- MaxProductOfCuttingRope.java
+++ MaxProductOfCuttingRope.java
@@ -1,4 +1,65 @@
+/**
+ * Given a rope with positive integer-length n, how to cut the rope into m integer-length parts with 
+ * length p[0], p[1], ...,p[m-1], in order to get the maximal product of p[0]*p[1]* ... *p[m-1]? 
+ * m is determined by you and must be greater than 0 (at least one cut must be made). 
+ * Return the max product you can have.
+ *
+ * Assumptions 
+ * n >= 2
+ *
+ * Examples
+ * n = 12, the max product is 3 * 3 * 3 * 3 = 81
+ * (cut the rope into 4 pieces with length of each is 3).
+ */
+
 public class MaxProductOfCuttingRope {
+  /** 
+   * Method 1: DFS (Recursion) + Backtracking
+   *
+   * (只考虑右边最后一刀)
+   *                                      maxP(5)
+   *          /                    |                    |                    \
+   * max{maxP(4), 4} * 1   max{maxP(3), 2} * 2   max{maxP(2), 2} * 3   max{maxP(1), 1} * 4
+   *     / | \                  /  \                    |                   0
+   *    ......                 ......                 ......
+   * 
+   * Time: O(n!) for n meters rope
+   * Space: O(n) for stack calls
+   */
+  public int maxProduct(int length) {
+    int n = length;
+    
+    // base case: no cut made 
+    if (n <= 1) {
+      return 0;
+    }
+    
+    int max = 0;
+    for (int i = 1; i < n; i++) {
+      int best = Math.max(maxProduct(n - i), n - i);
+      max = Math.max(i * best, max);
+    }
+    
+    return max;
+  }
+  
+  /** 
+   * Method 2: DFS (Recursion) + Backtracking
+   *
+   * (只考虑右边最后一刀)
+   *                                      maxP(5)
+   *          /                    |                    |                    \
+   * max{maxP(4), 4} * 1   max{maxP(3), 2} * 2   max{maxP(2), 2} * 3   max{maxP(1), 1} * 4
+   *     / | \                  /  \                    |                   0
+   *    ......                 ......                 ......
+   * 
+   * Time: O(n!) for n meters rope
+   * Space: O(n) for stack calls
+   */
+  
+  
+  
+  
   public int maxProduct(int length) {
     int n = length;
     int[] M = new int[n + 1];
