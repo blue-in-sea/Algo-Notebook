39c5b3de28944e6e2e4f94009d9e7c9c17c1833d
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-16 00:25:54 -0600

    Update DictionaryWord.java

--- DictionaryWord.java
+++ DictionaryWord.java
@@ -1,18 +1,31 @@
+/** 
+ * Assumption:
+ * input is not null or empty
+ * dict is not null and is not empty
+ * all the strings in dict are not null or empty
+ */
+
 public class DictionaryWord {
   /** 
    * M[i] represents whether the substring from the 0-th index to i-th index
    * can be composed by dictionary words
    *
-   * 	M[i] = M[j] && (M[j, i] is in dic?)
-	 *       左大段  &&  右小段
+   * M[i] = M[j] && (input[j, i] is in dic?)
+   *      左大段  &&  右小段
    */
   public boolean dictionaryWord(String input, String[] dict) {
-    Set<Integer> dictSet = toSet(dict);
-    int n = input.length;
-    boolean[] canBreak = new int[n + 1];
-    for (int i = 0; i < n + 1; i++) {
+    Set<String> dictSet = toSet(dict);
+    int n = input.length();
+    boolean[] canBreak = new boolean[n + 1];
+    canBreak[0] = true;
+    /**
+     * Note: sometimes it will be handy to have such index matching
+     * canBreak[i] -> represents index (i-1) in input
+     *             -> also the subString (0,i)
+     */
+    for (int i = 1; i < n + 1; i++) {
       for (int j = 0; j < i; j++) {
-        if (canBreak[j] && set.contains(input.substring(j, i))) {
+        if (canBreak[j] && dictSet.contains(input.substring(j, i))) {
           canBreak[i] = true;
           break;
         }
