18cde2ee67858472f0b7f731cbfde3af5a138769
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-16 14:02:32 -0600

    Update MaxProductOfCuttingRope.java

--- MaxProductOfCuttingRope.java
+++ MaxProductOfCuttingRope.java
@@ -45,50 +45,14 @@ public class MaxProductOfCuttingRope {
   
   /** 
    * Method 2: Dynamic Programming（左大段 + 右大段）
-   * 
-   * M[i] represents 储存前面的下刀，有几个地方能下最后一刀
-   * M[i,j] = MAX{ M[i,k] * M[k,j] } 
-   *
-   * Time: O(n!) = ½[(n-2)(n-3)(n-4)...1]
-   * Space: O(1) 
+   * M[i] represents the max product of (p[0], p[1], ...,p[m-1])
    */
-  
-  
-  // Space: O(n) to O(1) by only store the currMax 
-  public int maxProduct(int length) {
-    int n = length;
-    int[] M = new int[n + 1];
-
-    // base case
-    M[0] = 0;
-    M[1] = 0; 
-
-    // sub-solution: n meters
-    for (int i = 2; i <= n; i ++) {
-      int currMax = 0;
-      for (int j = 1; j <= i; j++) {
-        currMax = Math.max(currMax, Math.max(j, M[j]) * (i - j));
-      } 
-      
-      M[i] = currMax;
-    }
-    
-    return M[n];
-  } 
   
   /** 
    * Method 3: Dynamic Programming（左大段 + 右小段）Optimal!!
-   * 
-   * M[i] represents [within the range from the beginning index to i-th index]
-   * the max length of the ascending subarray. [must include the i-th element]
-   *
-   * M[i] = M[i - 1] + 1     a[i] > a[i - 1] 
-   *      = 1                otherwise
-   * Time: O(n) for linear scan, Space: O(1)
+   * M[i] represents the max product of (p[0], p[1], ...,p[m-1])
+   * 右小段: 右边最后一刀绳子本身的长度
    */
-  public int maxProduct(int length) {
-  }
-  
   
   
   
