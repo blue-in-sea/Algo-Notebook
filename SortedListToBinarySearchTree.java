b4abf04de1a5070a03c8963afd5102e0fb8f3239
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-17 14:26:01 -0600

    Update SortedListToBinarySearchTree.java

--- SortedListToBinarySearchTree.java
+++ SortedListToBinarySearchTree.java
@@ -1,3 +1,26 @@
+/**/
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *     left = right = null;
+ *   }
+ * }
+ */
 public class SortedListToBinarySearchTree {
 
 }
