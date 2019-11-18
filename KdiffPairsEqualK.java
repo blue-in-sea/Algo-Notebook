494a87d0af83969a5333505ae49ce8ba1fa812f6
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-18 16:45:10 -0600

    Update KdiffPairsEqualK.java

--- KdiffPairsEqualK.java
+++ KdiffPairsEqualK.java
@@ -1,9 +1,25 @@
 class KdiffPairsEqualK {
     // K = 0, not pass the test case
     public int findPairs(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
+        if (nums == null || nums.length == 0 || k < 0) {
             return 0;
         }
+/*
+        if (k == 0) {
+            Map<Integer, Integer> map0 = new HashMap<>();
+            int cnt0 = 0;
+            for (int i : nums) {
+                 map0.put(i, map0.getOrDefault(i, 0) + 1);
+            }
+            
+            for (Map.Entry<Integer, Integer> entry : map0.entrySet()) {
+                if (entry.getValue() >= 2) {
+                    cnt0++;
+                } 
+            }
+            return cnt0;
+        }       
+*/   
         // <K, V> : < num, used true or false
         Map<Integer, Boolean> map = new HashMap<>();
         
