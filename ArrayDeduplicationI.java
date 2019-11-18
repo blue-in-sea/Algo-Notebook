62aac01f653ed8c398cb6606a5f4031ec0fc198d
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-18 01:29:01 -0600

    Update ArrayDeduplicationI.java

--- ArrayDeduplicationI.java
+++ ArrayDeduplicationI.java
@@ -1,8 +1,9 @@
 /** 
  * s: the part on the left side of s (exclusive s) are processed are part
- * f: linear scan 
+ * f: linear scan
  * return array[s-1]
- *  
+ * 
+ * initialize s = f = 1
  * case 1: if a[f] == a[s-1], skip (duplicates)
  * case 2: if a[f] != a[s-1], a[s] = a[f], s++, f++
  */
