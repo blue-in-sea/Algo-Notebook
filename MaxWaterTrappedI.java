729ef7553197b5bc77653058cca1b0175a2e027f
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-17 22:53:41 -0600

    Update MaxWaterTrappedI.java

--- MaxWaterTrappedI.java
+++ MaxWaterTrappedI.java
@@ -1,4 +1,7 @@
 public class MaxWaterTrappedI {
+  // Optimization on Space: Two Pointers (leftMax + rightMax)
+  // Time: O(N)
+  // Space: O(1)
   public int maxTrapped(int[] array) {
     if (array == null || array.length == 0) {
       return 0;
@@ -20,5 +23,39 @@ public class MaxWaterTrappedI {
       }
     }
     return waterSum; 
+  }
+  
+  // Naive Idea: leftMax Arr + rightMax Arr + water Arr 
+  // Time: O(N)
+  // Space: O(3N)
+  public int maxTrapped(int[] array) {
+    // assume array is not null, and
+    // has no negative values
+    if (array.length == 0) {
+      return 0;
+    }
+
+    int[] leftMax = new int[array.length];
+    int lmax = 0;
+    for (int i = 0; i < array.length; i++) {
+      lmax = Math.max(lmax, array[i]);
+      leftMax[i] = lmax;
+    }
+
+    int[] rightMax = new int[array.length];
+    int rmax = 0;
+    for (int i = array.length - 1; i >= 0; i--) {
+      rmax = Math.max(rmax, array[i]);
+      rightMax[i] = rmax;
+    }
+
+    int[] water = new int[array.length];
+    int waterSum = 0;
+    for (int i = 0; i < array.length; i++) {
+      water[i] = Math.min(leftMax[i], rightMax[i]) - array[i];
+      waterSum += water[i];
+    }
+
+    return waterSum;
   }
 }
