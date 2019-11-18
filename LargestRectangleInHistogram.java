544ebe9c8d0ae875cc5432200acb5bc00fb06b40
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-17 21:36:54 -0600

    Update LargestRectangleInHistogram.java

--- LargestRectangleInHistogram.java
+++ LargestRectangleInHistogram.java
@@ -1,4 +1,6 @@
 public class LargestRectangleInHistogram {
+  // Time: O(N) for every single element can only be inserted and popped out of stack only once
+  // Space: O(N) for the stack 
   public int largest(int[] array) {
     // assume array is not null or empty
     // and contains no negative value
