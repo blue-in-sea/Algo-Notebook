6732b4db205112ed21cabcf3dd9a751398e1ca06
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-18 01:25:30 -0600

    Create ArrayDeduplicationI.java

---
+++ ArrayDeduplicationI.java
@@ -0,0 +1,40 @@
+// case 1: if a[f] == a[s-1], skip (duplicates)
+// case 2: if a[f] != a[s-1], a[s] = a[f], s++, f++
+
+public class ArrayDeduplicationI {
+  /**
+   * Given a !! sorted !! integer array, remove duplicate elements. 
+   * For each group of elements with the same value, keep only one of them. 
+   */
+  public int[] dedup(int[] array) {
+    if (array == null || array.length <= 1) {
+      return array;
+    }
+    int slow = 1;
+    for (int fast = 1; fast < array.length; fast++) {
+      if (array[slow - 1] != array[fast]) {
+        array[slow] = array[fast];
+        slow++;
+      }
+    }
+    return Arrays.copyOf(array, slow);
+  }
+  
+  /**
+   * Remove adjacent, repeated characters in a given string, leaving only one character 
+   * for each group of such characters.
+   */
+  public String deDup(String input) {
+    if (input == null || input.length() == 0) {
+      return input;
+    }
+    char[] array = input.toCharArray();
+    int slow = 1;
+    for (int fast = 1; fast < array.length; fast++) {
+      if (array[fast] != array[slow - 1]) {
+        array[slow++] = array[fast];
+      }
+    }
+    return new String(array, 0, slow);
+  }
+}
