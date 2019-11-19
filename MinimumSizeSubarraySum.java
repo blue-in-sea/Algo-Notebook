64ef8c1de77af9739101ba9d0a5ecf96c770af43
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-18 21:57:47 -0600

    Update MinimumSizeSubarraySum.java

--- MinimumSizeSubarraySum.java
+++ MinimumSizeSubarraySum.java
@@ -1,4 +1,5 @@
 /**
+ * 1.2.2 同向双指针1: Minimum size subarray sum (L460)
  * 同向双指针 l & r
  * 每次删除左指针左边的数字 只要当前sum小于s，右指针继续向右移动 时间复杂度O(N)
  */
@@ -7,6 +8,9 @@ public class MinimumSizeSubarraySum {
      * @param nums: an array of integers
      * @param s: An integer
      * @return: an integer representing the minimum size of subarray
+     *
+     * Time: O(N) for r moves n steps, l moves n steps, total add up to 2N
+     * Space: O(1) 
      */
   public int minimumSize(int[] nums, int s) {
         int sum = 0;
