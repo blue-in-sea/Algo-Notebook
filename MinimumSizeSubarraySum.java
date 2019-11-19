30a56166fa5f42a85a501f8551757fd1b547504e
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-18 22:11:49 -0600

    Update MinimumSizeSubarraySum.java

--- MinimumSizeSubarraySum.java
+++ MinimumSizeSubarraySum.java
@@ -1,6 +1,12 @@
 /**
  * 1.2.2 同向双指针1: Minimum size subarray sum (L460)
- * 同向双指针 l & r
+ * 首先你有两个指针, 都是往右
+ * 只有能证明算法符合: 右指针往右, 左指针不可往左(or 左指针只能不动或者往左) 的性质, prefer 同向双指针!!
+ */
+
+/**
+ * Algo 
+ * 同向双指针 l & r start from index 0
  * 每次删除左指针左边的数字 只要当前sum小于s，右指针继续向右移动 时间复杂度O(N)
  */
 public class MinimumSizeSubarraySum {
