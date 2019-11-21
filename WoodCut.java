2294c18cd7e8f8a4dfdd1c4062baf0c7213d76fd
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-21 17:09:40 -0600

    Update WoodCut.java

--- WoodCut.java
+++ WoodCut.java
@@ -36,4 +36,39 @@ public class WoodCut {
         }
         return cnt;
     }
+    
+    // version 2
+    public int woodCut(int[] L, int k) {
+        int l = 1;
+        int r = 0;
+        for (int item : L) {
+            r = Math.max(r, item);
+        }
+        
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (count(L, mid) >= k) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+        // post-processing
+        if (count(L, r) >= k) {
+            return r;
+        }
+        
+        if (count(L, l) >= k) {
+            return l;
+        }
+        return 0;
+    }
+    
+    private int count(int[] L, int len) {
+        int sum = 0;
+        for (int item : L) {
+            sum += item / len;
+        }
+        return sum;
+    }
 }
