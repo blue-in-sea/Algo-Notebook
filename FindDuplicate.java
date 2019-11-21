10cf031f873338c511eb4fc2443f6039a43fe9cb
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-21 17:58:33 -0600

    Update FindDuplicate.java

--- FindDuplicate.java
+++ FindDuplicate.java
@@ -4,6 +4,34 @@ public class FindDuplicate {
      * @return: the duplicate one
      */
     public int findDuplicate(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        int res = 0;
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (count(nums, mid) <= mid) {
+                l = mid + 1;
+            } else {
+                res = mid;
+                r = mid - 1;
+            }
+        }
+        
+        return res;
+    }
+    
+    private int count(int[] nums, int mid) {
+        int cnt = 0;
+        for (int item : nums) {
+            if (item <= mid) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    
+    // version 2
+    public int findDuplicate(int[] nums) {
         int l = 0;
         int r = nums.length - 1;
         
