48fd4a8e2d5e9321f9dfbd5d76c7d0b5f43839ec
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-21 16:48:41 -0600

    Update SquareRootI.java

--- SquareRootI.java
+++ SquareRootI.java
@@ -1,21 +1,5 @@
 public class SquareRootI {
-  public int sqrt(int x) {
-    long res = 1;
-    long left = 0, right = x;  // in order to pass <0>, left must be initialed at the lowest value, or res declared as 0, either works
-
-    while (left <= right) {
-      long mid = left + (right - left) / 2;
-      if (mid * mid <= x) {
-        res = mid;
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-
-    return (int) res;
-  }
-  
+  // version 1
   public int sqrt(int x) {
     // assume n is guaranteed to be >= 0
     long left = 1;
@@ -33,5 +17,23 @@ public class SquareRootI {
       return (int) right;  // can pass <0> case, since right = 0
     }   
     return (int) left;
+  }
+  
+  // version 2
+  public int sqrt(int x) {
+    long res = 1;
+    long left = 0, right = x;  // in order to pass <0>, left must be initialed at the lowest value, or res declared as 0, either works
+
+    while (left <= right) {
+      long mid = left + (right - left) / 2;
+      if (mid * mid <= x) {
+        res = mid;
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return (int) res;
   }
 }
