d569f90e210d77c785ec4b626085639b6b0c5418
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-21 16:19:07 -0600

    Create SquareRootII.java

---
+++ SquareRootII.java
@@ -0,0 +1,24 @@
+public class SquareRootII {
+    /**
+     * @param x: a double
+     * @return: the square root of x
+     */
+    public double sqrt(double x) {
+        double l = 0;
+        double r = Math.max(x, 1.0);
+        double eps = 1e-12;
+        double res = 0;
+        
+        while (l <= r) {
+            double mid = l + (r - l) / 2;
+            if (mid * mid <= x) {
+                res = mid;
+                l = mid + eps;
+            } else {
+                r = mid - eps;
+            }
+        }
+        
+        return res;
+    }
+}
