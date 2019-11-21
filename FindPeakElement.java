5a871390c8f5bfea03e93d7aee2f37b2ba35a488
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-21 14:25:26 -0600

    Update FindPeakElement.java

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -12,11 +12,11 @@ public class FindPeakElement {
         while (left + 1 < right) {
             int mid = left + (right - left) / 2;
       
-            //如果中间的数比后一位数大的话，peek点肯定在mid左边或是mid。
-            if (A[mid] > A[mid + 1]) {
-                right = mid;
-            } else {
+            //如果中间的数比后一位数小的话，peek点肯定在mid右边包括mid
+            if (A[mid] < A[mid + 1]) {
                 left = mid;
+            } else {
+                right = mid;
             }
         }
         // while 结束时 left 和 right 相邻
@@ -25,6 +25,30 @@ public class FindPeakElement {
         } else {
             return left;
         }
+        // 谁大 return 谁
+    }
+    
+    public int findPeak(int[] A) {
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        
+        int left = 1, right = A.length - 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+     
+            if (A[mid] < A[mid + 1]) {
+                left = mid + 1;
+            } else {
+                if (A[mid] > A[mid - 1]) {
+                    return mid;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        // while 结束时 left 和 right 相错一步
+        return A[left] > A[right] ? left : right;
         // 谁大 return 谁
     }
 }
