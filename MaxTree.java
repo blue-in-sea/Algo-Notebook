63d81153467cdcf3376ce2b3f53d2d24d6638948
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-23 01:06:39 -0600

    Update MaxTree.java

--- MaxTree.java
+++ MaxTree.java
@@ -16,7 +16,7 @@ public class MaxTree {
       //如果i==length,新建节点设置值为无穷大，否则值为A[i]
       TreeNode cur = i == A.length ? new TreeNode(Integer.MAX_VALUE) : new TreeNode(A[i]);
       while (!stack.isEmpty()) {                //如果栈不为空
-        if (cur.key > stack.peek().key) {			  //如果新建节点的值比栈顶大
+        if (cur.key > stack.peek().key) {       //如果新建节点的值比栈顶大
           TreeNode node = stack.pop();          //临时保存栈顶节点并弹出
           if (stack.isEmpty()) {                //如果栈为空
             cur.left = node;                    //临时保存的栈顶的节点是当前新建节点的左子树
