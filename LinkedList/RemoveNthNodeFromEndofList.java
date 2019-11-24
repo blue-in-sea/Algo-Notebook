bc9ceec1ea61f8b137d01f88ad5acb49375a6179
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-24 00:36:08 -0600

    Update RemoveNthNodeFromEndofList.java

--- LinkedList/RemoveNthNodeFromEndofList.java
+++ LinkedList/RemoveNthNodeFromEndofList.java
@@ -8,6 +8,33 @@
  *   }
  * }
  */
-public class RemoveNthNodeFromEndofList {
-	
+public class Solution {
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode fast = head;
+	  ListNode slow = head;
+
+    while (n > 0 && fast != null) {
+      fast = fast.next;
+      n--;
+    }
+    // Check if n is greater than the size of linked List 
+    if (n > 0) {
+      return head;
+    }
+
+    // Check if the linked list has only one node
+	  if (fast == null) {
+      return head.next;
+    }
+
+	  while (fast.next != null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+	  
+    // Remove slow.next which is the nth form the end
+	  slow.next = slow.next.next;
+	  return head;
+  }
 }
+
