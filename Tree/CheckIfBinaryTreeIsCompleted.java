3004f3cdcb637dcbbd0b16cca3178bd15581ca5c
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-24 21:48:12 -0600

    Update CheckIfBinaryTreeIsCompleted.java

--- Tree/CheckIfBinaryTreeIsCompleted.java
+++ Tree/CheckIfBinaryTreeIsCompleted.java
@@ -9,48 +9,36 @@
  * }
  */
 public class CheckIfBinaryTreeIsCompleted {
-  /**
- * public class TreeNode {
- *   public int key;
- *   public TreeNode left;
- *   public TreeNode right;
- *   public TreeNode(int key) {
- *     this.key = key;
- *   }
- * }
- */
-public class CheckIfBinaryTreeIsCompleted {
   public boolean isCompleted(TreeNode root) {
     if (root == null) {
       return true;
-    }
-    Queue<TreeNode> q = new LinkedList<>();
+    } 
+    Queue<TreeNode> q = new LinkedList<TreeNode>();
     boolean flag = false;
     q.offer(root);
     while (!q.isEmpty()) {
-      TreeNode n = q.poll(); 
-      
+      TreeNode n = q.poll(); // n represnts curNode
+      // if any of child is not present,
+      // set the flag to TURE
       if (n.left == null) {
-        // if any of child is not present before, we set the flag to red (true)
         flag = true;
       } else if (flag) {
-        // if flag is set (from previous node), we still see n has a left child 
-        // we return flase 
+        // if flag is set but we still see curNode has 
+        // a left child we return FALSE
         return false;
       } else {
         q.offer(n.left);
       }
-      
       if (n.right == null) {
         flag = true;
       } else if (flag) {
-        // if flag is set (from n's left node), we still see n has a right child
-        // we return false
+        // if flag is set but we still see curNode has 
+        // a right child we return FALSE
         return false;
       } else {
         q.offer(n.right);
       }
     }
-    return true;
+    return true;    
   }
 }
