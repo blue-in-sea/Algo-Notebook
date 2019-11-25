d2fc0d7ea24273c6c9d1b1181e239e8a494afbcc
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-24 21:46:47 -0600

    Update CheckIfBinaryTreeIsCompleted.java

--- Tree/CheckIfBinaryTreeIsCompleted.java
+++ Tree/CheckIfBinaryTreeIsCompleted.java
@@ -9,7 +9,48 @@
  * }
  */
 public class CheckIfBinaryTreeIsCompleted {
-
-
-
+  /**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class CheckIfBinaryTreeIsCompleted {
+  public boolean isCompleted(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    Queue<TreeNode> q = new LinkedList<>();
+    boolean flag = false;
+    q.offer(root);
+    while (!q.isEmpty()) {
+      TreeNode n = q.poll(); 
+      
+      if (n.left == null) {
+        // if any of child is not present before, we set the flag to red (true)
+        flag = true;
+      } else if (flag) {
+        // if flag is set (from previous node), we still see n has a left child 
+        // we return flase 
+        return false;
+      } else {
+        q.offer(n.left);
+      }
+      
+      if (n.right == null) {
+        flag = true;
+      } else if (flag) {
+        // if flag is set (from n's left node), we still see n has a right child
+        // we return false
+        return false;
+      } else {
+        q.offer(n.right);
+      }
+    }
+    return true;
+  }
 }
