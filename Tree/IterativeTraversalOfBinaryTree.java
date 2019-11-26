6b9ff4b7f5a91fb76e9db3e4d3c2bb16d389a219
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-25 22:19:27 -0600

    Update IterativeTraversalOfBinaryTree.java

--- Tree/IterativeTraversalOfBinaryTree.java
+++ Tree/IterativeTraversalOfBinaryTree.java
@@ -21,6 +21,68 @@
  * }
  */
 public class IterativeTraversalOfBinaryTree {
+  /**
+   * Pre-order Traversal Of Binary Tree (iterative)
+   * Time: O(N), Space: O(H)
+   */
+  public List<Integer> preOrder(TreeNode root) {
+    // Time: O(N), Space: O(H)
+    List<Integer> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+    
+    Deque<TreeNode> stack = new LinkedList<>();	
+    pushLeft(root, stack, res);
+	  while (!stack.isEmpty()) {
+		  TreeNode cur = stack.pollFirst();
+		  cur = cur.right;
+		  pushLeft(cur, stack, res);
+	  }
+    return res;
+  }
+
+  private void pushLeft(TreeNode cur, Deque<TreeNode> stack, List<Integer> res) {
+    while (cur != null) {
+      res.add(cur.key);
+      stack.offerFirst(cur);
+      cur = cur.left;
+    } // cur = null -> end while
+  }
+  
+  /**
+   * In-order Traversal Of Binary Tree (iterative)
+   * Time: O(N), Space: O(H)
+   */
+  public List<Integer> inOrder(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+    
+    Deque<TreeNode> stack = new LinkedList<>();	
+    pushLeft(root, stack);
+	  while (!stack.isEmpty()) {
+		  TreeNode cur = stack.pollFirst();
+		  res.add(cur.key);
+		  cur = cur.right;
+		  pushLeft(cur, stack);
+	  }
+    return res;
+  }
+
+  private void pushLeft(TreeNode cur, Deque<TreeNode> stack) {
+    while (cur != null) {
+      stack.offerFirst(cur);
+      cur = cur.left;
+    } // cur = null -> end while
+  }
+  
+  /**
+   * Post-order Traversal Of Binary Tree (iterative)
+   * Time: O(N), Space: O(H)
+   */
+  
  
 
 }
