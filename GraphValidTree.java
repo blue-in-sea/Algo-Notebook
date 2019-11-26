1f3a2be28abae542f31f4d8e5f8e91e7f04e5f5e
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-26 15:32:52 -0600

    Create GraphValidTree.java

---
+++ GraphValidTree.java
@@ -0,0 +1,29 @@
+public class GraphValidTree {
+
+  // Method 2: Union Find
+  public boolean validTree(int n, int[][] edges) {
+    // intialize n isolate islands
+    int[] nums = new int[n];
+    Arrays.fill(nums, -1);
+
+    // perform union find
+    for (int i = 0; i < edges.length; i++) {
+      int x = find(nums, edges[i][0]);
+      int y = find(nums, edges[i][1]);
+
+      // if two vertices happen to be in the same set
+      // then there's a cycle
+      if (x == y) return false;
+
+      // union
+      nums[x] = y;
+    }
+
+    return edges.length == n - 1;
+  }
+
+  private int find(int[] nums, int i) {
+    if (nums[i] == -1) return i;
+    return find(nums, nums[i]);
+  }
+}
