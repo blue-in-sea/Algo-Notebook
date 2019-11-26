882b1c5edf9f7bd27b0855fb57251f5b0b856a3d
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-26 16:12:18 -0600

    Update GraphValidTree.java

--- GraphValidTree.java
+++ GraphValidTree.java
@@ -1,6 +1,54 @@
 public class GraphValidTree {
+  
+  // Method 2: DFS
+  public boolean validTree(int n, int[][] edges) {
+    // initialize adjacency list
+    List<List<Integer>> adjList = new ArrayList<List<Integer>>(n);
 
-  // Method 2: Union Find
+    // intialize vertices
+    for (int i = 0;  i < n; i++) {
+      adjList.add(i, new ArrayList<Integer>());
+    }
+
+    // add edges: undirected graph (each edge is a pair of nodes)
+    for (int i = 0; i < edges.length; i++) {
+      int u = edges[i][0], v = edges[i][1];
+      adjList.get(u).add(v);
+      adjList.get(v).add(u);
+    }
+
+    boolean[] visited = new boolean[n];
+
+    // make sure there is no circle 
+    if (hasCircle(adjList, 0, visited, -1)) {
+      return false;
+    }
+
+    // make sure all vertices are connected
+    for (int i = 0; i < n; i++) { 
+      if (!visited[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private boolean hasCircle(List<List<Integer>> adjList, int u, boolean[] visited, int parent) {
+    visited[u] = true;
+
+    for (int i = 0; i < adjList.get(u).size(); i++) {
+      int v = adjList.get(u).get(i);
+
+      if ((visited[v] && parent != v) || (!visited[v] && (!visited[v] && hasCircle(adjList, v, visited, u)))) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // Method 3: Union Find
   public boolean validTree(int n, int[][] edges) {
     // intialize n isolate islands
     int[] nums = new int[n];
