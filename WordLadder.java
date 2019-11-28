e9fa593d62a607821886558fd1a7d85fe0b3ae62
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-27 20:34:05 -0600

    Update WordLadder.java

--- WordLadder.java
+++ WordLadder.java
@@ -1,64 +1,72 @@
-public class Solution {
-  public int ladderLength(String start, String end, List<String> dict) {
-    if (dict == null) {
-      return 0;
-    }
-
-    if (start.equals(end)) {
-      return 1;
+public class WordLadder {
+    /*
+     * @param start: a string
+     * @param end: a string
+     * @param dict: a set of string
+     * @return: An integer
+     */
+    public int ladderLength(String start, String end, Set<String> dict) {
+        if (dict == null) {
+            return 0;
+        }
+        
+        if (start.equals(end)) {
+            return 1;
+        }
+        
+        dict.add(start);
+        dict.add(end);
+        
+        HashSet<String> visited = new HashSet<String>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start);
+        visited.add(start);
+        
+        int length = 1;
+        while (!queue.isEmpty()) {
+            length++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                for (String nextWord : getNextWords(word, dict)) {
+                    if (visited.contains(nextWord)) {  // dedup
+                        continue;
+                    }
+                    if (nextWord.equals(end)) {
+                        return length;
+                    }
+                    
+                    queue.offer(nextWord);
+                    visited.add(nextWord);
+                }
+            }
+        }
+        
+        return 0;
     }
     
-    // start word and end word already are given in dict
-
-    Set<String> visited = new HashSet<>();
-    Queue<String> queue = new LinkedList<>();
-
-    queue.offer(start);
-    visited.add(start);
-
-    int step = 1;
-    while(!queue.isEmpty()) {
-      step++;
-      int size = queue.size();
-      for (int i = 0; i < size; i++) {
-        String cur = queue.poll();
-        for (String nextWord : getNextWords(cur, dict)) {
-          if (visited.contains(nextWord)) {
-            continue;
-          }
-
-          if (nextWord.equals(end)) {
-            return step;
-          }
-
-          queue.offer(nextWord);
-          visited.add(nextWord);
-        }
-      }
+    // replace character of a string 
+    private String replace(String s, int index, char c) {
+        char[] word = s.toCharArray();
+        word[index] = c;
+        return new String(word);
     }
-
-    return 0;  // if no path found
-  }
-
-  private String replace(String s, int index, char c) {
-    char[] word = s.toCharArray();
-    word[index] = c;
-    return new String(word);
-  }
-
-  private List<String> getNextWords(String word, List<String> dict) {
-    List<String> list = new ArrayList<>();
-    for (char c = 'a'; c <= 'z'; c++) {
-      for (int i = 0; i < word.length(); i++) {
-        if (c == word.charAt(i)) {
-          continue;
+    
+    // get connections with given word
+    // given word = 'hot', dict = {'hot', 'hit', 'hog'}, it will return ['hit', 'hog']
+    private List<String> getNextWords(String word, Set<String> dict) {
+        List<String> nextWords = new ArrayList<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < word.length(); i++) {
+                if (c == word.charAt(i)) {
+                    continue;
+                }
+                String nextWord = replace(word, i, c);
+                if (dict.contains(nextWord)) {
+                    nextWords.add(nextWord);
+                }
+            }
         }
-        String nextWord = replace(word, i, c);
-        if (dict.contains(nextWord)) {
-          list.add(nextWord);;
-        }
-      }
+        return nextWords;
     }
-    return list;
-  }
 }
