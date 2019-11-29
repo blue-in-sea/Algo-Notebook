a90ac49b562b848fec53d7b8b49ca34eb589283f
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-28 21:18:11 -0600

    Create KthSmallestElementInBinarySearchTree.java

---
+++ KthSmallestElementInBinarySearchTree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class KthSmallestElementInBinarySearchTree {
+    /**
+     * @param root: the given BST
+     * @param k: the given k
+     * @return: the kth smallest element in BST
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        pushLeft(root, stack);
+        
+        for (int i = 0; i < k - 1; i++) {
+            TreeNode cur = stack.pollFirst();
+            cur = cur.right;
+            pushLeft(cur, stack);
+        }
+        
+        return stack.peekFirst().val;
+    }
+    
+    private void pushLeft(TreeNode cur, Deque<TreeNode> stack) {
+        while (cur != null) {
+            stack.offerFirst(cur);
+            cur = cur.left;
+        }
+    }
+}
