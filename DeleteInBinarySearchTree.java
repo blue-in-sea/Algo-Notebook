54cae1cfab60f9c58a42a5cc8f7dca1c733e9565
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-30 15:17:23 -0600

    Update DeleteInBinarySearchTree.java

--- DeleteInBinarySearchTree.java
+++ DeleteInBinarySearchTree.java
@@ -9,6 +9,15 @@
  * }
  */
 public class DeleteInBinarySearchTree {
+  /**
+   * case 1: no L, no R
+   * case 2: no L, yes R
+   * case 3: yes L, no R
+   * case 4: yes L, yes R
+   *
+   * Note that case 2 should combine with case 4
+   * instead of combining with case 1!!!
+   */
   public TreeNode deleteTree(TreeNode root, int key) {
     if (root == null) {
       return null;
@@ -23,6 +32,48 @@ public class DeleteInBinarySearchTree {
         return root.right;
       } else {
         TreeNode newRoot = deleteSmallest(root.right);
+        newRoot.left = root.left;
+        newRoot.right = root.right; 
+        return newRoot;
+      }
+    }
+    if (key < root.key) {
+      root.left = deleteTree(root.left, key);
+    } else if (key > root.key) {
+      root.right = deleteTree(root.right, key);
+    }
+    return root;
+  }
+
+  private TreeNode deleteSmallest(TreeNode root) {
+    while (root.left.left != null) {
+      root = root.left;
+    }
+    TreeNode smallest = root.left;
+    root.left = root.left.right;
+    return smallest;
+  }
+}
+
+
+/**
+ * reorgnize the case classifiers 
+ */
+public class Solution {
+  public TreeNode deleteTree(TreeNode root, int key) {
+    if (root == null) {
+      return null;
+    }
+    if (key == root.key) {
+      if (root.left == null && root.right == null) {     /* case 1: no L, no R .   */
+        return root.right;                           
+      } else if (root.right == null) {                   /* case 3: yes L, no R    */
+        return root.left;
+      } else if (root.right.left == null) {              /* case 4: yes L, yes R   */
+        root.right.left = root.left;                     /* (case 2:  no L, yes R) */
+        return root.right;
+      } else {                                           /* case 4: yes L, yes R   */
+        TreeNode newRoot = deleteSmallest(root.right);   /* (case 2:  no L, yes R) */
         newRoot.left = root.left;
         newRoot.right = root.right; 
         return newRoot;
