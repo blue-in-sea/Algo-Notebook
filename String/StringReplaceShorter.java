5d9ecf1fb96538865ff59c2dc2fa13b2a11a95e5
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-12-01 16:16:31 -0600

    Create StringReplaceShorter.java

---
+++ String/StringReplaceShorter.java
@@ -0,0 +1,51 @@
+public class StringReplaceShorter {
+  // Method 1: not using any String/StringBuilder utility,
+  // and using char[] to do it in-place.
+  public String replace(String input, String s, String t) {
+    // Assumptions: input, s, t are not null, s is not empty
+    char[] array = input.toCharArray();
+/*    
+    if (s.length() < t.length()) {
+      replaceLonger(array, s, t);
+    }
+*/
+    return replaceShorter(array, s, t);
+  }
+
+  private String replaceShorter(char[] array, String s, String t) {
+    // we reuse the input char array since we do not need expand
+    // the original array size
+    // slow and fast pointer both move from left to right direction
+    int slow = 0;
+    int fast = 0;
+    while (fast < array.length) {
+      // when we find a match of s on the substring starting from 
+      // the fast pointer, we copy the t at slow pointer 
+      if (fast <= array.length - s.length() && equalSubstring(array, fast, s)) {
+        copySubstring(array, slow, t);
+        slow += t.length();
+        fast += s.length();
+      } else {
+        array[slow++] = array[fast++];
+      }
+    }
+    return new String(array, 0, slow);
+  }
+
+  // check if the substring starting at fromIdx is same as s (pattern)
+  private boolean equalSubstring(char[] array, int fromIdx, String s) {
+    for (int i = 0; i < s.length(); i++) {
+      if (array[fromIdx + i] != s.charAt(i)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // copy the string t to result at fromIdx
+  private void copySubstring(char[] array, int fromIdx, String t) {
+    for (int i = 0; i < t.length(); i++) {
+      array[fromIdx + i] = t.charAt(i);
+    }
+  }
+}
