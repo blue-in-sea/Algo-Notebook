5ee3858bf527e5b6d55f8654d5eafd325a9873d7
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-12-02 19:45:09 -0600

    Update ValidateBinarySearchTree.java

--- BST/ValidateBinarySearchTree.java
+++ BST/ValidateBinarySearchTree.java
@@ -1,4 +1,14 @@
 public class ValidateBinarySearchTree {
+    public void deleteEdge(TreeNode root) {
+	    if(root == null) return;
+	    root = dfs(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
 
-
+    private TreeNode dfs(TreeNode root, int left, int right) {
+	    if(root == null) return null;
+	    if(root.val <= left || root.val >= right) return null;
+	    root.left = dfs(root.left, left, root.val);
+	    root.right = dfs(root.right, root.val, right);
+	    return root;
+    }
 }
