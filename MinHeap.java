751dfc0b18a77f9bb37c8e568ef06308d8c81968
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-23 19:56:08 -0600

    Update MinHeap.java

--- MinHeap.java
+++ MinHeap.java
@@ -1,12 +1,28 @@
 public class MinHeap {
-  private int[] array;
-  private int size;
+    private int[] array;
+    private int size;
   
-  public MinHeap(int[] array) {
-  
-  
-  
-  }
+    public MinHeap(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegaArgumentException("input array can not be null or empty");
+        }
+        
+        this.array = array;
+        size = array.length;
+        heapify();
+    }
+    
+    private void heapify() {
+        for (int lastIdx = size / 2 - 1; lastIdx >= 0; lastIdx--) {
+            percolateDown(lastIdx);
+        }
+    }
+    
+    public MinHeap(int cap) {
+        if (cap <= 0) [
+            
+        
+    }
 
 
 
