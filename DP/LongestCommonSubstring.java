c2e37d001aae94c4a3b12817f5f97e1b98f81de7
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-04-22 13:18:21 -0500

    Update LongestCommonSubstring.java

--- DP/LongestCommonSubstring.java
+++ DP/LongestCommonSubstring.java
@@ -1,3 +1,14 @@
+/**
+ * Find the longest common substring of two given strings.
+ * 
+ * Assumptions
+ * The two given strings are not null     
+ *     
+ * Examples       
+ * S = “abcde”, T = “cdf”, the longest common substring of S and T is “cd” 
+ * S = “student”, T = “sweden”, the longest common substring of S and T is “den”    
+ */
+
 public class LongestCommonSubstring {
   // Version 1 : DP
   // M[i][j] = lonest common substring between the first i of a[i]
@@ -26,3 +37,20 @@ public class LongestCommonSubstring {
     return s.substring(start, start + maxLen);
   }
 }
+
+/**
+ *     (i) 0 1 2 3 4 5 6 7
+ * (j)     0 s t u d e n t
+ *  0      0 0 0 0 0 0 0 0           // base case: M[0][j] = 0
+ *  1   s  0 1 0 0 0 0 0 0 
+ *  2   w  0 0 0 0 0 0 0 0
+ *  3   e  0 0 0 0 0 1 0 0
+ *  4   d  0 0 0 0 1 0 0 0
+ *  5   e  0 0 0 0 0 2 0 0
+ *  6   n  0 0 0 0 0 0 3 0          // index_i = 6, index_j = 6, maxLen = 3.   start = 6 - 3 = 3, end = 3 + 3 = 6
+ * // base case M[i][0] = 0         // return s.substring(start = 3 inclusive, end = 6 exclusive);
+ */
+
+// public String substring(int beginIndex, int endIndex)
+// beginIndex - the beginning index, inclusive.
+// endIndex   - the ending index, exclusive.
