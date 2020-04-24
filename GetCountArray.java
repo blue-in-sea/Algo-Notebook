bfa4b3caa8512fb479379efc923a99623494c481
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-04-23 22:54:44 -0500

    Create GetCountArray.java

---
+++ GetCountArray.java
@@ -0,0 +1,14 @@
+/**
+ * Given a target integer T and an integer array A sorted in ascending order, 
+ * find the index i such that A[i] == T or return -1 if there is no such index. 
+ *
+ * Assumption: There can be duplicate elements in the array, 
+ * and you can return any of the indices i such that A[i] == T.
+ */
+
+public class GetCountArray {
+  
+
+
+
+}
