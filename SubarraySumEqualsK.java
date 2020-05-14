61b11750b22df294c4da0e33ca8fcd53ac2a27e0
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-05-14 00:53:20 -0500

    Update SubarraySumEqualsK.java

--- SubarraySumEqualsK.java
+++ SubarraySumEqualsK.java
@@ -1,4 +1,52 @@
-public class Solution {
+public class SubarraySumEqualsK {
+  // Methdod 1: Sliding Window
+  // Find subarraySum(i, j) == target, increment the cnt
+  // Time: O(n), Space: O(1)
+  public int numOfSubarraySumToK(int[] nums, int k) {
+    if (nums == null || nums.length == 0) {
+      return -1;
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < nums.length; i++) {
+      int sum = nums[i];
+      if (sum == k) cnt++;
+
+      for (int j = i + 1; j < nums.length; j++) {
+        sum += nums[j];
+        if (sum == k) cnt++;
+      }
+    }
+
+    return cnt;
+  }
+  
+  // Methdod 2: DP to store the pre_sum
+  // DP[i] represents the pre_sum including a[i]
+  public int numOfSubarraySumToK(int[] nums, int k) {
+    if (nums == null || nums.length == 0) {
+      return -1;
+    }
+
+    int cnt = 0;
+    int[] dp = new int[nums.length];
+    dp[0] = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      dp[i] = dp[i - 1] + nums[i];
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      if (dp[i] == k) cnt++;
+
+      for (int j = i + 1; j < nums.length; j++) {
+        if (dp[j] - dp[i] == k) cnt++;
+      }
+    }
+
+    return cnt;
+  }
+  
   // Methdod 3: DP pre_sum + HashMap <K = pre_sum, V = Freq>
   // DP[i] represents the pre_sum including a[i]
   // Time: O(n), Space: O(n)
