99072a78dea9faa51e41f0c8390aedf7956e1e2d
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-05-15 12:57:35 -0500

    Update BSTemplate.java

--- BinarySearch/BSTemplate.java
+++ BinarySearch/BSTemplate.java
@@ -1,23 +1,47 @@
 
 public class BSTemplate {
-    // soln-1
+    // template-1: with post-processing
     public int lowerBound(int[] array, int target) {
-        if (array == null || array.length == 0) 
+        if (array == null || array.length == 0) {
             return 0;
-        
-        int l = 0, r = array.length;
+        }
+        int len = array.length;
+        int l = 0, r = len - 1;
         while (l < r - 1) {
             int m = l + (r - l) / 2;
-            if (array[mid] >= target) {
-                r = mid;
+            if (array[m] >= target) {
+                r = m;
             } else {
-                l = mid;
+                l = m; 
+            }
+        }
+        if (array[l] >= target) {
+            return l;
+        }
+        if (array[r] >= target) {
+            return r;
+        } 
+        return len;
+    }
+    
+    // template-2: no post-processing
+    public int lowerBound(int[] array, int target) {
+
+        int len = array.length;
+        int l = 0, r = len;
+        while (l < r) {
+            if (array[m] >= target) {
+                r = m;
+            } else {
+                l = m + 1;
             }
         }
         
-                
-        
-    }
+        return l;     
+    } 
+    
+    // template-4: 
+}
   
   
 }
