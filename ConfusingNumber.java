1289b5f2385a7300da90008cbf7461f1d3332d66
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-08 18:48:45 -0500

    Update ConfusingNumber.java

--- ConfusingNumber.java
+++ ConfusingNumber.java
@@ -1,3 +1,22 @@
+/**
+ * Some of the digits can be rotated by 180 degrees to form new digits.
+ *
+ * e.g. When 0, 1, 6, 8, 9 are rotated 180 degrees,
+ * they become 0, 1, 9, 8, 6 respectively.
+ *
+ * When 2, 3, 4, 5 and 7 are rotated 180 degrees,
+ * they become invalid.
+ *
+ * A confusing number is a number that when rotated 180 degrees becomes a
+ * different number with each digit valid.
+ * (Note that the rotated number can be greater than the original number.)
+ *
+ * Given a positive integer N, return ALL the confusing numbers between 1 and N inclusive.
+ * 
+ * Input: 6, Output: True
+ * We get 9 after rotating 6, 9 is a valid number and 9!=6.
+ */
+
 public ConfusingNumber {
     public static boolean confusingNumberMap(int num) {
         Map<Integer, Integer> map = new HashMap();
