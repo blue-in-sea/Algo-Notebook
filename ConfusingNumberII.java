5eafa280c3ca5ff0cb6117bc1b61b2ca9d7d7e7b
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-08 19:40:09 -0500

    Update ConfusingNumberII.java

--- ConfusingNumberII.java
+++ ConfusingNumberII.java
@@ -1,10 +1,20 @@
-public class ConfusingNumberII {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println(confusingNumberII(20));
-        /** when N = 20, res = [10, 16, 18, 19, 6, 9], cnt = 6 */
-    }
+/**
+ * Some of the digits can be rotated by 180 degrees to form new digits.
+ *
+ * e.g. When 0, 1, 6, 8, 9 are rotated 180 degrees,
+ * they become 0, 1, 9, 8, 6 respectively.
+ *
+ * When 2, 3, 4, 5 and 7 are rotated 180 degrees,
+ * they become invalid.
+ *
+ * A confusing number is a number that when rotated 180 degrees becomes a
+ * different number with each digit valid.
+ * (Note that the rotated number can be greater than the original number.)
+ *
+ * Given a positive integer N, return how many confusing numbers are between 1 and N inclusive.
+ */
 
+public class ConfusingNumberII {
     public static int confusingNumberII(int N) {
         int[] arr = {0,1,6,8,9};
         int[] map = {0,1,9,8,6};
