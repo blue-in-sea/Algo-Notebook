094ed355450cf2a20eae4ceda41cb29d8ba4cffe
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-23 23:47:01 -0500

    Update 3SumClosest.java

--- 2Sum/3SumClosest.java
+++ 2Sum/3SumClosest.java
@@ -3,9 +3,9 @@ class 3SumClosest {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int res = nums[0] + nums[1] + nums[2];
-        for(int i = 0;i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
             int left = i + 1, right = nums.length - 1;
-            while(left < right) {
+            while (left < right) {
                 int sum = nums[left] + nums[right] + nums[i];
                 // update the result sum
                 if(Math.abs(target - sum) < Math.abs(target - res)) {
