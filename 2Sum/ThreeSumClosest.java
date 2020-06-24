9a0fbc2ef4eb7ee139e13161584307a29342a7e2
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-23 23:53:55 -0500

    Update and rename 3SumClosest.java to ThreeSumClosest.java

--- 2Sum/ThreeSumClosest.java
+++ 2Sum/ThreeSumClosest.java
@@ -1,11 +1,11 @@
-class 3SumClosest {
+class ThreeSumClosest {
     // essence: search in sorted matrix
     // time: O(nlogn) + O(n^2) = O(n^2)
-    // space: O(1)ntwo pointers
+    // space: O(1) since two pointers
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int res = nums[0] + nums[1] + nums[2];
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length - 2; i++) {
             int left = i + 1, right = nums.length - 1;
             while (left < right) {
                 int sum = nums[left] + nums[right] + nums[i];
