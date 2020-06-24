2c4c0165cb066a90422887b081cdf350a9b0873a
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-23 23:42:27 -0500

    Create 3SumClosest.java

---
+++ 2Sum/3SumClosest.java
@@ -0,0 +1,26 @@
+class 3SumClosest {
+    // Essence: search in a sorted matrix
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[2];
+        for(int i = 0;i < nums.length; i++) {
+            int left = i + 1, right = nums.length - 1;
+            while(left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                // update the result sum
+                if(Math.abs(target - sum) < Math.abs(target - res)) {
+                    res = sum;
+                } 
+                // two pointers 
+                if(sum > target) {
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
