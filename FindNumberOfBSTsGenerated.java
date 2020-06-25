8c911226f219f93ef97c9d7d5f37a0109fb12f14
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-25 01:08:13 -0500

    Create FindNumberOfBSTsGenerated.java

---
+++ FindNumberOfBSTsGenerated.java
@@ -0,0 +1,12 @@
+public class FindNumberOfBSTsGenerated {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1]*dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
