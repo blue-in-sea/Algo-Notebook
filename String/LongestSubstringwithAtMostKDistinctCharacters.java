67eebfc8bee959d0529776fe7c63d0d4e6da3418
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-26 01:29:10 -0500

    Update LongestSubstringwithAtMostKDistinctCharacters.java

--- String/LongestSubstringwithAtMostKDistinctCharacters.java
+++ String/LongestSubstringwithAtMostKDistinctCharacters.java
@@ -1,6 +1,44 @@
 public class LongestSubstringwithAtMostKDistinctCharacters {
-    // Version 2
-    // sliding window with frequency_map 
+    // Version-1
+    // Sliding window with cnt_array
+    // [l, r] represents the sliding window that contain at most k distinct char
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if (s == null || s.length() == 0 || k == 0) {
+            return 0;
+        }
+        
+        int left = 0, right = 0, unique = 0;
+        int cnt[] = new int[256];
+        int res = 0;
+        
+        while (right < s.length()) {
+            // expand right border: when the char at right has  
+            // the first appearrance in the sliding window
+            // unique++ and cnt frequency++
+            if (cnt[s.charAt(right)] == 0) {
+                unique++;
+            }
+            cnt[s.charAt(right)]++;
+            right++;
+            
+            // when the window contains more than k distinct char
+            // shrink left then update unique and cnt_table
+            while (unique > k) {
+                cnt[s.charAt(left)]--;
+                if (cnt[s.charAt(left)] == 0) {
+                    unique--;
+                }
+                left++;
+            }
+            
+            // update the global_max of the window's length
+            res = Math.max(res, right - left);
+        }
+        
+        return res;
+    }
+    // Version-2
+    // Sliding window with frequency_map 
     // [l, r] represents the sliding window that contain at most k distinct char
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         
