5c976a4d5c5c501a6bad998f68c231e346bb0ba2
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-07-03 19:30:14 -0500

    Update LongestValidParentheses.java

--- LongestValidParentheses.java
+++ LongestValidParentheses.java
@@ -21,6 +21,43 @@ public class LongestValidParentheses {
         return max;
     }
     
+    
+    // Method 2: 计数器 l & r
+    // Time: O(2n) -> O(n), Space: O(1)
+    public int longestValidParentheses(String s) {
+        int l = 0, r = 0, max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')  {
+                l++;
+            } else {
+                r++;
+            }
+
+            if (l == r) {
+                max = Math.max(max, 2 * r);
+            } else if (r > l) {
+                l = r = 0;
+            }
+        }
+
+        l = r = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                l++;
+            } else {
+                r++;
+            }
+
+            if (l == r) {
+                max = Math.max(max, 2 * l);
+            } else if (l > r) {
+                l = r = 0;
+            }
+        }
+
+        return max;
+    }
+    
     // Method 3: Dynamic Programming 
     // Time: O(n), Space: O(n)
     public int longestValidParentheses(String s) {
