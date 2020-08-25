e7f896101e9a5dadc8660d8294f809d3a2864550
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-08-25 00:53:14 -0500

    Create Trie.java

---
+++ Trie.java
@@ -0,0 +1,62 @@
+class TrieNode {
+    public char val;
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+    
+    public TrieNode() {}
+    public TrieNode(char c) {
+        TrieNode node = new TrieNode();
+        node.val = c;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        ();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode(c);
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (curr.children[c - 'a'] == null) return false;
+            curr = curr.children[c - 'a'];
+        }
+        return curr.isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if(curr.children[c - 'a'] == null) return false;
+            curr = curr.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
