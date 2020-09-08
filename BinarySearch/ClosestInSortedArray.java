f1c22a60a1c488e0fa0685863ddaf217f8c5cfee
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-09-08 05:09:50 -0500

    Update ClosestInSortedArray.java

--- BinarySearch/ClosestInSortedArray.java
+++ BinarySearch/ClosestInSortedArray.java
@@ -1,1 +1,25 @@
-
+public class ClosestInSortedArray {
+    public int closest(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        
+        int l = 0, r = array.length - 1;
+        while (l < r - 1) {
+            int m = l + (r - l) / 2;
+            if (array[m] == target) {
+                return m;
+            } else if (array[m] < target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        
+        if (Math.abs(array[l] - target) <= Math.abs(array[r] - target)) {
+            return l;
+        } else {
+            return r;
+        }
+    }
+}
