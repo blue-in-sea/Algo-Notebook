be114e48695c95ccabb67dc04b1d425085d8dfd6
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-09-11 20:55:09 -0500

    Update IsSubsequence.java

--- String/IsSubsequence.java
+++ String/IsSubsequence.java
@@ -14,4 +14,16 @@ public class IsSubsequence {
         }
         return false;
     }
+    
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        int i = 0;
+        for (int j = 0; j < t.length(); j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                if (i == s.length()) return true;
+            }
+        }
+        return false;
+    }
 }
