a714d9adca5a813fee0fe9dc318fa2039d333437
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-11-13 16:57:44 -0600

    Update DeepCopyUndirectedGraph.java

--- DeepCopyUndirectedGraph.java
+++ DeepCopyUndirectedGraph.java
@@ -27,7 +27,7 @@ class DeepCopyUndirectedGraph {
     }
     
     
-    // ======= for unconnected graph =======
+    // ======= DFS for unconnected graph =======
     public List<GraphNode> copy(List<GraphNode> graph) {
         if (graph == null) {
             return null;
@@ -50,6 +50,40 @@ class DeepCopyUndirectedGraph {
                 DFS(nei, map);
             }
             copy.neighbors.add(map.get(nei));
+        }
+    }
+    
+    // ======= BFS for unconnected graph =======
+    public List<GraphNode> copy(List<GraphNode> graph) {
+        if (graph == null) {
+            return null;
+        }
+        List<GraphNode> result = new ArrayList<>(); // copy graph 
+        Deque<GraphNode> queue = new ArrayDeque<>();
+        Map<GraphNode, GraphNode> map = new HashMap<>();
+        for (GraphNode node : graph) {
+            if (!map.containsKey(node)) {
+                map.put(node, new GraphNode(node.key));
+                queue.offer(node);
+                bfs(map, queue);
+            }
+            result.add(map.get(node));
+        }
+        return result;
+    }
+
+    private void bfs(Map<GraphNode, GraphNode> map, Deque<GraphNode> queue) {
+        while (!queue.isEmpty()) {
+            GraphNode curr = queue.poll();
+
+            for (GraphNode nei : curr.neighbors) {
+                if (!map.containsKey(nei)) {
+                    map.put(nei, new GraphNode(nei.key));
+                    queue.offer(nei);
+                }
+                // make the connection on copied graph
+                map.get(curr).neighbors.add(map.get(nei));
+            }
         }
     }
 }
