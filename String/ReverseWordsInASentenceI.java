8706afe56470e7ba19ab0137a544802bb13924ee
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-11-13 18:28:33 -0600

    Update ReverseWordsInASentenceI.java

--- String/ReverseWordsInASentenceI.java
+++ String/ReverseWordsInASentenceI.java
@@ -1,36 +1,37 @@
 public class ReverseWordsInASentenceI {
-  // Time: O(N)
-  // Space: O(1)
-  public String reverseWords(String input) {
-    // Assumptions:
-    // 1) The words are seperated by one space character
-    // 2) There is no leading or tailing space
-    // 3) input is not null
-    char[] array = input.toCharArray();  // convert to char array, solve in-place
-    reverse(array, 0, array.length - 1); // 1. reverse the entir char array
+    public String reverseWords(String s) {
+        // Assumptions:
+        // 1) The words are separated by one space character
+        // 2) If there is a leading or trailing space: (keep) or (trim)
+        // 3) input is not null
 
-    int start = 0;
-    // 2. reverse the each of the words 
-    for (int i = 0; i < array.length; i++) {
-      // the start index of a word 
-      if (array[i] != ' ' && (i == 0 || array[i - 1] == ' ')) {
-        start = i;
-      }
-      // the end index of word
-      if (array[i] != ' ' && (i == array.length - 1 || array[i + 1] == ' ')) {
-        reverse(array, start, i);
-      }
-    }
-    return new String(array);
-  }
+        // since string is immutable, we best to convert it to char array 
+        // when doing the manipulation on the given string 
+        char[] array = s.toCharArray();
 
-  private void reverse(char[] array, int left, int right) {
-    while (left < right) {
-        char temp = array[left];
-        array[left] = array[right];
-        array[right] = temp;
-        left++;
-        right--;
+        reverse(array, 0, s.length() - 1);
+
+        int start = 0;
+        // 2. reverse the each of the words 
+        for (int i = 0; i < array.length; i++) {
+            // find the start index of each word 
+            if (array[i] != ' ' && (i == 0 || array[i - 1] == ' ')) start = i;
+            // move i to the end index of this word and 
+            // reverse this segment of the sentence 
+            if (array[i] != ' ' && (i == array.length - 1 || array[i + 1] == ' ')) reverse(array, start, i);
+        }
+        
+        return new String(array);
+
     }
-  }
+
+    private void reverse(char[] array, int i, int j) {
+        while (i < j) {
+            swap(array, i++, j--);
+        }
+    }
+    
+    // ===== LC test case =====
+
+  
 }
