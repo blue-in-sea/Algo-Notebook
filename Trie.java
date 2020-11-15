6f9f2bfde7c10e14d962ef4ee79e510b4225ab71
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-11-15 13:48:05 -0600

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -22,6 +22,76 @@
  * All inputs are guaranteed to be non-empty strings.
  */
 
+// ========== HashMap Version =========
+
+class TrieNode {
+    public boolean isWord;
+    public Map<Character, TrieNode> children;
+
+    public TrieNode() {
+        this.children = new HashMap<>();
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();  
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        // corner case
+        if (word == null) return;
+
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        // for (char l: word.toCharArray()) current = current.getChildren().computeIfAbsent(l, c -> new TrieNode()); 
+        curr.isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) return false;
+            curr = curr.children.get(c);
+        }
+        return curr.isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (!curr.children.containsKey(c)) return false;
+            curr = curr.children.get(c);
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
+
+// ========== ARR VERSION =========
+
 class TrieNode {
     public char val;
     public boolean isWord;
@@ -76,11 +146,3 @@ public class Trie {
         return true;
     }
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
