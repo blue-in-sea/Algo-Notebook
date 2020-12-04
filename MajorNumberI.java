7e0233131fb1a97c5dd4dd79437274ccf96bbd7d
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-12-04 16:57:43 -0600

    Create MajorNumberI.java

---
+++ MajorNumberI.java
@@ -0,0 +1,46 @@
+class MajorNumberI {
+    public int majorityElement(int[] nums) {
+        int majorityCount = nums.length / 2;
+        
+        for (int num : nums) {
+            int count = 0;
+            for (int elem : nums) {
+                if (elem == num) {
+                    count += 1;
+                }
+            }
+            if (count > majorityCount) return num;
+        }
+        
+        return -1;
+    }
+    
+    
+    public int majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        Map<Integer, Integer> map = frequencyMap(nums);
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (majorityEntry == null || e.getValue() > majorityEntry.getValue()) {
+                majorityEntry = e;
+            }
+        }
+        return majorityEntry.getKey();  
+    }
+    
+    public Map<Integer, Integer> frequencyMap(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for (int ele : nums) {
+            if (!map.containsKey(ele)) {
+                map.put(ele, 1);
+            } else {
+                map.put(ele, map.get(ele) + 1);
+            }
+        }
+        return map;
+    } 
+}
