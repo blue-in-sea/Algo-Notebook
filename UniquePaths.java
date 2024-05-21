30221f25fc24a3d28baa5819548eaf6281357885
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-05-21 12:46:57 -0700

    Create UniquePaths.java

---
+++ UniquePaths.java
@@ -0,0 +1,18 @@
+class UniquePaths {
+    // Time: O(NM)
+    // Space: O(NM)
+
+    public int uniquePaths(int m, int n) {
+        int[][] d = new int[m][n];
+
+        for (int[] arr : d) {
+            Arrays.fill(arr, 1);
+        }
+        for (int col = 1; col < m; ++col) {
+            for (int row = 1; row < n; ++row) {
+                d[col][row] = d[col - 1][row] + d[col][row - 1];
+            }
+        }
+        return d[m - 1][n - 1];
+    }
+}
