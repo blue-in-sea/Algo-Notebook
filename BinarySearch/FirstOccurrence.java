b1ccde5365641f639de0ca79eb2c1e5fe5f3cf52
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-05-27 21:00:00 -0700

    Update FirstOccurrence.java

--- BinarySearch/FirstOccurrence.java
+++ BinarySearch/FirstOccurrence.java
@@ -1,3 +1,19 @@
+/**
+ * Given a target integer T and an integer array A sorted in ascending order, find the index of the first occurrence 
+ * of T in A or return -1 if there is no such index.
+ *
+ * Assumptions
+ * There can be duplicate elements in the array.
+ * 
+ * Examples
+ * A = {1, 2, 3}, T = 2, return 1
+ * A = {1, 2, 3}, T = 4, return -1
+ * A = {1, 2, 2, 2, 3}, T = 2, return 1
+ * Corner Cases
+ *
+ * What if A is null or A of zero length? We should return -1 in this case.
+ */
+
 public class FirstOccurrence {
   public int firstOccur(int[] array, int target) {
     if (array == null || array.length == 0) {
@@ -5,8 +21,11 @@ public class FirstOccurrence {
     }
     int left = 0;
     int right = array.length - 1;
-
-    while (left < right) {
+    // We need to use left < right - 1 here to make sure there is no infinite loop.
+    // think about the case when left = right - 1,
+    // then mid = left, it will be possiable picking [mid right] for next round,
+    // and it will go into a infinite loop in this case
+    while (left < right - 1) {
       int mid = left + (right - left) / 2;
       if (array[mid] == target) {
         right = mid;  // since we ask for 1st occurrence
@@ -16,10 +35,18 @@ public class FirstOccurrence {
         right = mid;
       }
     }
-    
+    // Make sure you understand all the possible situation when entering 
+    // this post-processing procedure
+    // 1. array only has one element 
+    // 2. array only has two elements
+    // 3. left == right - 1 and left is the result
+    // 4. left == right - 1 and right is the result
+    // 5. left == right - 1 and none of both is the result
     if (array[left] == target) {
       return left;
-    } 
+    } else if (array[right] == target) {
+      return right;
+    }
     return -1;
   }
 }
