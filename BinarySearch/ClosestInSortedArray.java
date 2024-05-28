d524cbf8990d2606764419f4a958d4b97a1a24d2
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-05-27 20:54:36 -0700

    Update ClosestInSortedArray.java

--- BinarySearch/ClosestInSortedArray.java
+++ BinarySearch/ClosestInSortedArray.java
@@ -1,3 +1,17 @@
+/**
+ * Given a target integer T and an integer array A sorted in ascending order, find the index i in A such that A[i] is
+ * closest to T.
+ *
+ * Assumptions
+ * There can be duplicate elements in the array, and we can return any of the indices with same value.
+ * Examples
+ *
+ * A = {1, 2, 3}, T = 2, return 1
+ * A = {1, 4, 6}, T = 3, return 1
+ * A = {1, 4, 6}, T = 5, return 1 or 2
+ * A = {1, 3, 3, 4}, T = 2, return 0 or 1 or 2
+ */
+
 public class ClosestInSortedArray {
     public int closest(int[] array, int target) {
         if (array == null || array.length == 0) {
