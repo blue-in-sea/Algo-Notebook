24d9ac839402370ff06cf230d59876f25d81c62f
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-05-30 10:57:34 -0700

    Create DistanceKInBinaryTree.java

---
+++ DistanceKInBinaryTree.java
@@ -0,0 +1,7 @@
+class DistanceKInBinaryTree {
+    // https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/
+     
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        
+    }
+}
