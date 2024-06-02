a73c8cfb394a4c078390c31fa6decc08a44680c5
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-02 14:46:04 -0700

    Create MeetingRooms.java

---
+++ Merge-Interval/MeetingRooms.java
@@ -0,0 +1,30 @@
+class MeetingRooms {
+    // Algo: 扫描线
+    // Data Str: TreeMap<k: time, v: cnt of the meeting room at this time> (sorted hashmap)
+    // * search & insertion & deletion: O(logn)
+    // * Time complexity to build a treemap: O(nlogn)
+    // * Space complexity O(n) same with hashmap
+
+    public int minMeetingRooms(int[][] intervals) {
+        // corner case
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        for (int[] interval : intervals) {
+            // if start time
+            map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+            // if end time
+            map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
+        }
+
+        int max = 0;
+        int sum = 0;
+        for (int cnt : map.values()) {
+            sum += cnt;
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+}
