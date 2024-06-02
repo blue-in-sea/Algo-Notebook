6fbd9b71d1fa962e9dc779dd73f3099bb21bf9d4
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-01 19:52:33 -0700

    Update BinaryTreeLevelOrderTraversal.java

--- Tree/BinaryTreeLevelOrderTraversal.java
+++ Tree/BinaryTreeLevelOrderTraversal.java
@@ -2,10 +2,11 @@
  * Definition of TreeNode:
  * public class TreeNode {
  *     public int val;
- *     public TreeNode left, right;
+ *     public TreeNode left;
+       public TreeNode right;
  *     public TreeNode(int val) {
  *         this.val = val;
- *         this.left = this.right = null;
+ *         // this.left = this.right = null;
  *     }
  * }
  */
