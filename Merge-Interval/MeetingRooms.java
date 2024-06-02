88a7c027b62e3f57a9ca477e2e3ffefe3c5ade7b
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-02 14:48:24 -0700

    Update MeetingRooms.java

--- Merge-Interval/MeetingRooms.java
+++ Merge-Interval/MeetingRooms.java
@@ -1,9 +1,13 @@
 class MeetingRooms {
     // Algo: 扫描线
+
     // Data Str: TreeMap<k: time, v: cnt of the meeting room at this time> (sorted hashmap)
     // * search & insertion & deletion: O(logn)
     // * Time complexity to build a treemap: O(nlogn)
     // * Space complexity O(n) same with hashmap
+
+    // 1. build the treemap (sort the time, keep track of the number of rooms needed at the given time)
+    // 2. travser the treemap to find the max # of overlap intervals
 
     public int minMeetingRooms(int[][] intervals) {
         // corner case
@@ -19,7 +23,7 @@ class MeetingRooms {
             map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
         }
 
-        int max = 0;
+        int max = 0; // max # of overlap intervals == min rooms needed 
         int sum = 0;
         for (int cnt : map.values()) {
             sum += cnt;
