85b0cdce1a55bd95586cee8653b67483b53460ad
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-02 15:12:54 -0700

    Update InsertInterval.java

--- Merge-Interval/InsertInterval.java
+++ Merge-Interval/InsertInterval.java
@@ -25,32 +25,38 @@
  * 	 |_ _ _ _ _|			
  */
 
-class public InsertInterval {  
-
-
-
-    // version 2: different input and output form 
+class InsertInterval {
+    // Time: O(n) given the input intervals were sorted 
+    // Space: O(1) no extra space used 
     public int[][] insert(int[][] intervals, int[] newInterval) {
-         List<int[]> result = new ArrayList<>();
-        
-         for(int[] i : intervals){
-             if(newInterval == null || i[1] < newInterval[0]){
-                 result.add(i);
-             } else if(i[0] > newInterval[1]){
+        List<int[]> res = new ArrayList<>();
+        for(int[] curInterval : intervals){
+            // case 1: insertion of the newInterval does not needed any more 
+            // (or) cur.end < new.start 
+            if(newInterval == null || curInterval[1] < newInterval[0]){
+                res.add(curInterval);
+            } 
+            // case 2: cur.start > new.end
+            else if(curInterval[0] > newInterval[1]){
                 // be carefult the sequence here
-                 result.add(newInterval);
-                 result.add(i);
-                 newInterval = null;
-             } else {   
-                 newInterval[0] = Math.min(newInterval[0], i[0]);//get min
-                 newInterval[1] = Math.max(newInterval[1], i[1]);//get max
-             }
-         }
-        
-        if(newInterval != null) {
-            result.add(newInterval);
+                res.add(newInterval);
+                res.add(curInterval);
+                newInterval = null; // already inserte, set newInterval to be null
+            } 
+            // case 3: merge 2 overlap intervals
+            else {   
+                // new start time will be the min
+                newInterval[0] = Math.min(newInterval[0], curInterval[0]);//get min
+                // new end time will be the min
+                newInterval[1] = Math.max(newInterval[1], curInterval[1]);//get max
+            }
         }
         
-        return result.toArray(new int[result.size()][]);
-    }    
+        // post-process: where the interval will be on the last end 
+        if(newInterval != null) {
+            res.add(newInterval);
+        }
+        
+        return res.toArray(new int[res.size()][]);
+    } 
 }
