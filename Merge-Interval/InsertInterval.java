698c8d69133ec158e303ce41666e328fb6419b98
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-02 15:17:40 -0700

    Update InsertInterval.java

--- Merge-Interval/InsertInterval.java
+++ Merge-Interval/InsertInterval.java
@@ -1,33 +1,49 @@
 /**
- * ___: current interval; _ _ _: newInterval
+ * 57. Insert newInterval
+ * Insert newInterval into intervals such that intervals is still sorted in ascending order by start_i and
+ * intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
  *
- * 1) cur.end < newInterval.start，then we can safely add i to result;
- * 	  newInterval still needs to be compared with latter intervals
- * 
- * 	  |________|
- * 			         |_ _ _ _ _|
- * 			
- * 2) cur.start > newInterval.end，then we can safely add both to result，
- * 	  and mark newInterval as null
- * 	
- * 			         |________|
- * 	  |_ _ _ _ _|
- * 			
- * 3) There is overlap between i and newInterval. We can merge curInterval into newInterval, 
- * then use the updated newInterval to compare with latter intervals.
- * [case 1]	
- * 	  |________|
- * 		  |_ _ _ _ _|
+ * Return intervals after the insertion.
+ * Note that you don't need to modify intervals in-place. You can make a new array and return it.
  *
- * [case 2]	  			
- *            
- * 		 |________|
- * 	 |_ _ _ _ _|			
+ * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * Output: [[1,5],[6,9]]
+ * Example 2:
+ *
+ * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * Output: [[1,2],[3,10],[12,16]]
+ * Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
  */
-
 class InsertInterval {
     // Time: O(n) given the input intervals were sorted 
     // Space: O(1) no extra space used 
+
+   /**
+     * ___: current interval; _ _ _: newInterval
+     *
+     * 1) cur.end < newInterval.start，then we can safely add i to result;
+     * 	  newInterval still needs to be compared with latter intervals
+     *
+     * 	  |________|
+     * 			         |_ _ _ _ _|
+     *
+     * 2) cur.start > newInterval.end，then we can safely add both to result，
+     * 	  and mark newInterval as null
+     *
+     * 			         |________|
+     * 	  |_ _ _ _ _|
+     *
+     * 3) There is overlap between i and newInterval. Merge curInterval into newInterval: res.start = min(cur.start, new.start) & res.start = max(cur.end > new.end)
+     * then use the updated newInterval (res) to compare with latter intervals.
+     * [case 1]	
+     * 	  |________|
+     * 		  |_ _ _ _ _|
+     *
+     * [case 2]	  			
+     *
+     * 		 |________|
+     * 	 |_ _ _ _ _|			
+     */
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res = new ArrayList<>();
         for(int[] curInterval : intervals){
