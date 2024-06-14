253918d925709991f1c605866717d0462ffa6d08
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-13 22:47:11 -0700

    Update JumpGame.java

--- DP/JumpGame.java
+++ DP/JumpGame.java
@@ -14,26 +14,25 @@
  */
 
 class JumpGame {
-    // DP: from end to start
-    // Time: O(n^2), Space: O(n)
+    // boolean dp[i] represents can jump from i-th element to the target
+    // base case:
+    //     dp[4] = true since a[4] is target itself
+    // induction rule:
+    //     dp[i] = true if (there exists one) j such that dp[j] == true
+    //                     where i <= j <= i + a[i]
+    //           = false otherwise
 
+    // index  0  1  2  3  4
+    // Arr  [ 2, 3, 1, 1, 4 ]
+    //        <-
+    // dp   [ T, T, T, T, T ]
+    
+    // DP1: from end to start 如上图
+    // Time: O(n^2), Space: O(n)
     public boolean canJump(int[] array) {
         // Assume the input is not null and input has length at leaste 1
         int len = array.length;
         if (len == 1) return true;
-
-        // boolean dp[i] represents can jump from i-th element to the target
-        // base case:
-        //     dp[4] = true since a[4] is target itself
-        // induction rule:
-        //     dp[i] = true if (there exists one) j such that dp[j] == true
-        //                     where i <= j <= i + a[i]
-        //           = false otherwise
-
-        // index  0  1  2  3  4
-        // Arr  [ 2, 3, 1, 1, 4 ]
-        //        <-
-        // dp   [ T, T, T, T, T ]
 
         boolean[] dp = new boolean[len];
         // dp[len - 1] = true;  // base can skip, since default is true
