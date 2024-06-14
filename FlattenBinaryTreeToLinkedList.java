dd017c71c1a48d1e3793011e1f179bf7caf2286a
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-14 12:22:15 -0700

    Update FlattenBinaryTreeToLinkedList.java

--- FlattenBinaryTreeToLinkedList.java
+++ FlattenBinaryTreeToLinkedList.java
@@ -9,67 +9,82 @@
  * }
  */
 public class FlattenBinaryTreeToLinkedList {
-  /**
-   * Method 1: Traversal (not recommend!)
-   */
-  private TreeNode lastNode = null; // declare a global var
-  public TreeNode flatten(TreeNode root) {
-    if (root == null) {
-      return null;
-    }
-  
-    helper(root);
-    return root;
-  }
-  private void helper(TreeNode root) {
-    if (root == null) {
-      return;
+    public void flatten(TreeNode root) {
+        TreeNode[] prev = new TreeNode[1];
+        dfs(root, prev);
     }
 
-    if (lastNode != null) {
-      lastNode.left = null;
-      lastNode.right = root;  // here we modify the right child
+    private void dfs(TreeNode root, TreeNode[] prev) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.right, prev);
+        dfs(root.left, prev);
+        root.right = prev[0];
+        root.left = null;
+        prev[0] = root;
+    }
+    /**
+     * Method 1: Traversal (not recommend!)
+     */
+    private TreeNode lastNode = null; // declare a global var
+    public TreeNode flatten(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        helper(root);
+        return root;
+    }
+    private void helper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (lastNode != null) {
+            lastNode.left = null;
+            lastNode.right = root;  // here we modify the right child
+        }
+
+        lastNode = root;
+        TreeNode right = root.right; // right child's reference would be lost 
+        // if we do not save it before modify the root.right
+        helper(root.left);
+        helper(right);
     }
 
-    lastNode = root;
-    TreeNode right = root.right; // right child's reference would be lost 
-    // if we do not save it before modify the root.right
-    helper(root.left);
-    helper(right);
-  }
-  
-  /**
-   * Method 2: Divide and Conquer
-   */
-  public TreeNode flatten(TreeNode root) {
-    helper(root);
-    return root;
-  }
-
-  // flatten root and return the last node
-  public TreeNode helper(TreeNode root) {
-    if (root == null) {
-      return null;
+    /**
+     * Method 2: Divide and Conquer
+     */
+    public TreeNode flatten(TreeNode root) {
+        helper(root);
+        return root;
     }
 
-    TreeNode leftLast = helper(root.left);
-    TreeNode rightLast = helper(root.right);
+    // flatten root and return the last node
+    public TreeNode helper(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
 
-    // connect leftLast to root.right
-    if (leftLast != null) {
-      leftLast.right = root.right;
-      root.right = root.left;
-      root.left = null;
+        TreeNode leftLast = helper(root.left);
+        TreeNode rightLast = helper(root.right);
+
+        // connect leftLast to root.right
+        if (leftLast != null) {
+            leftLast.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+
+        if (rightLast != null) {
+            return rightLast;
+        }
+
+        if (leftLast != null) {
+            return leftLast;
+        }
+
+        return root;
     }
-
-    if (rightLast != null) {
-      return rightLast;
-    }
-
-    if (leftLast != null) {
-      return leftLast;
-    }
-
-    return root;
-  }
 }
