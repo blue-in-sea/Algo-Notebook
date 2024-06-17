6fcda665c64e10a48b51336438e89c44af245fe4
blue.sea <tldr.code@gmail.com>
2024-06-17 16:39:41 -0700

    Update LargestRectangleInHistogram.java

--- MonoStack/LargestRectangleInHistogram.java
+++ MonoStack/LargestRectangleInHistogram.java
@@ -4,22 +4,10 @@
  * return the area of the largest rectangle in the histogram.
  *
  * Example 1:
- *                   0 1 2 3 4 5    6
- * Input: heights = [2,1,5,6,2,3]   x
+ * Input: heights = [2,1,5,6,2,3]
  * Output: 10
  * Explanation: The above is a histogram where width of each bar is 1.
  * The largest rectangle is shown in the red area, which has an area = 10 units.
- *
- *  peak                (i -  l) *  h[i]
- * stack [0]        2 = (1 - 0)  *   2
- * stack [3, 2, 1]  6 = (4 - 3)  *   6
- *                 10 = (4 - 2)  *   5 maxArea
- *
- * stack [5, 4, 1] 3 = (6 - 5) * 3
- *                 8 = (6 - 2) * 2
- *
- * stack [6]
- *
  *
  * Example 2:
  * Input: heights = [2,4]
