e55267fe1b7f34641fc762d0a3a49f0111297465
blue.sea <tldr.code@gmail.com>
2024-06-18 16:47:52 -0700

    Update ReconstructItinerary.java

--- ReconstructItinerary.java
+++ ReconstructItinerary.java
@@ -1,44 +1,86 @@
+/**
+ * 332. Reconstruct Itinerary
+ * You are given a list of airline tickets where tickets[i] = [start_i, end_i] represent the departure and the arrival airports of one flight.
+ * Reconstruct the itinerary in order and return it.
+ *
+ * Requirement:
+ * 1. Always start with "JFK"
+ * 2. For multiple valid itineraries, return the route with the smallest lexical order
+ *    (i.e.)n["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"]
+ * 3. All tickets form at least one valid itinerary. You must use all the tickets once and only once.
+ *
+ * Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+ * Output: ["JFK","MUC","LHR","SFO","SJC"]
+ *
+ * Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+ * Explanation: ["JFK","SFO","ATL","JFK","ATL","SFO"] is another possible reconstruction;
+ *               but it is larger in lexical order.
+ */
 class ReconstructItinerary {
-   /**
-     * You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+    /**
+     * The problem can be modelled as a directed graph:
+     * Vertex -> airport
+     * Edge   -> ticket[start, end] with direction
      *
-     * All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+     * Our general intuition is to jump to graph algorithms like DFS/BFS.
+     * The DFS/BFS algorithms help us traverse all nodes in the graph, but fail to cover all edges in the graph
      *
-     * For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
-     * You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+     * This is where Eulerian Path comes into play. An Eulerian Path is a list of all edges in a graph in a sequence
+     * such that you can go from point A to all other nodes in the graph multiple times,
+     * as long as an edge is not visited.
+     *
+     * (Implementation wise, when visited a node, we poll the edge from the graph)
+     * https://leetcode.com/problems/reconstruct-itinerary/discuss/1161401/C%2B%2B-Explaination%3A-Transition-from-DFS-to-Eulerian-Path
      */
-  
-    
-    List<String> route = new ArrayList<>();
-   Map<String, PriorityQueue<String>> targets = new HashMap<>();
-   
+
+    // Time: O(E * log(E/V)) where V vertex, E edges
+    // Space: O(V + E)
+
+
     public List<String> findItinerary(List<List<String>> tickets) {
-        for (List<String> ticket : tickets) {
-            targets.computeIfAbsent(ticket.get(0), k -> new PriorityQueue()).add(ticket.get(1));
+        // corner case
+        if (tickets == null || tickets.size() == 0) {
+            return null;
         }
-        dfs("JFK");
+
+        // build graph: <key = Node, value = List of children sorting by lexical order>
+        Map<String, PriorityQueue<String>> graph = buildGraph(tickets);
+        // construct a path when traversing the graph
+        List<String> route = new ArrayList<>();
+        dfs("JFK", route, graph);
         return route;
     }
-    
-    public void dfs(String airport) {
-       // if the we have node, and this node still has its nei
-        while(targets.containsKey(airport) && !targets.get(airport).isEmpty()) {
-            dfs(targets.get(airport).poll());
+
+    public void dfs(String airport, List<String> route, Map<String, PriorityQueue<String>> graph) {
+        // if the we have node, and this node still has its children
+        while(graph.containsKey(airport) && !graph.get(airport).isEmpty()) {
+            dfs(graph.get(airport).poll(), route, graph);
         }
         route.add(0, airport);
     }
-   
-   
-   /**
-     * The problem can be modelled as a graph, with each node representing a city, and each ticket representing an edge from source city to destination city.
-     * The problem asks us to find a connected sequence(path) consisting of all edges in a graph, starting with "JFK".
-     *
-     * Our general intuition is to jump to algorithms like DFS/BFS. The DFS/BFS algorithms help us traverse all nodes in the graph, but fail to cover all edges in the graph
-     *
-     * This is where Eulerian Path comes into play. An Eulerian Path is a list of all edges in a graph in a sequence such that you can go from point A to all other nodes in the graph multiple times,
-     * as long as an edge is not visited.
-     *
-     * https://leetcode.com/problems/reconstruct-itinerary/discuss/1161401/C%2B%2B-Explaination%3A-Transition-from-DFS-to-Eulerian-Path
-     *
-     */
+
+    // build the graph
+    private Map<String, PriorityQueue<String>> buildGraph(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            graph.computeIfAbsent(ticket.get(0), k -> new PriorityQueue()).add(ticket.get(1));
+        }
+        return graph;
+    }
 }
+
+// Advance: Complexity Analysis
+// 1. Time:
+// - complexity of the DFS function would be O(E)
+// - but before DFS, we use PQ to sort all edges, which dominates the overall complexity **
+//   - 1. (worst cae) graph is unbalance:
+//         connections are centered in a single airport, JFK will have half of the flights -> V >>> E/2
+//         Time: O(VlogV) to sort all nodes this will dominate
+//   - 2. (normal case) graph is balance
+//         # of airports has equal # of outgoing flights -> 1 airport ~= E/(2V) of flights (include return flights)
+//         Time: O(E * log(E/V))
+// 2. Space:
+// - graph size: O(V + E)
+// - stack calls: O(E)
+// - total: O(V + 2E) = O(V + E)
