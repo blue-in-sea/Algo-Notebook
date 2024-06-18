4bdcc3bb237f5e4bcc9ee10906abd8c1a119cd9b
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-18 10:45:18 -0700

    Update DiceRollsWithTargetSum.java

--- Recursion/DiceRollsWithTargetSum.java
+++ Recursion/DiceRollsWithTargetSum.java
@@ -1,4 +1,5 @@
 /**
+ * 1155. Number of Dice Rolls With Target Sum
  * You have n dice, and each dice has k faces numbered from 1 to k.
  * Given three integers n, k, and target, return the number of possible ways (out of the kn total ways) to roll the dice,
  * so the sum of the face-up numbers equals target. Since the answer may be too large, return it modulo 109 + 7.
