6099c22f5a400c10ceeb9734c55b0525ef60a9a2
blue.sea <tldr.code@gmail.com>
2024-06-19 02:18:29 -0700

    Update MaxWaterTrapped.java

--- DP/MaxWaterTrapped.java
+++ DP/MaxWaterTrapped.java
@@ -1,7 +1,17 @@
+package DP;
+
+/**
+ * 42. Trapping Rain Water
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * compute how much water it can trap after raining.
+ *
+ * Input: height = [4,2,0,3,2,5]
+ * Output: 9
+ */
 public class MaxWaterTrapped {
-  // Optimization on Space: Two Pointers (leftMax + rightMax)
+  // Optimization on Space: Two Pointers (leftMax + rightMax) + waterSum
   // Time: O(N)
-  // Space: O(1)
+  // Space: O(1) Only constant space for l, r, lmax, rmax
   public int maxTrapped(int[] array) {
     if (array == null || array.length == 0) {
       return 0;
@@ -13,10 +23,12 @@ public class MaxWaterTrapped {
     int rmax = array[r];
     while (l < r) {
       if (array[l] <= array[r]) {
+        // 木桶原理：water trapped 取决于短板：左边界
         waterSum += Math.max(0, lmax - array[l]);
         lmax = Math.max(lmax, array[l]);
         l++;
       } else {
+        // 木桶原理：water trapped 取决于短板：右边界
         waterSum += Math.max(0, rmax - array[r]);
         rmax = Math.max(rmax, array[r]);
         r--;
@@ -24,8 +36,17 @@ public class MaxWaterTrapped {
     }
     return waterSum; 
   }
+  /**
+   * index            0 1 2 3 4 5
+   * Input: height = [4,2,0,3,2,5], Output: 9
+   * l: 0, r: 5, arr[l]: 0, arr[r]: 5, lmax: 4, rmax: 5, sum: 0, maxWaterSum: 0
+   * l: 1, r: 5, arr[l]: 1, arr[r]: 5, lmax: 4, rmax: 5, sum: 2, maxWaterSum: 2
+   * l: 2, r: 5, arr[l]: 2, arr[r]: 5, lmax: 4, rmax: 5, sum: 4, maxWaterSum: 6
+   * l: 3, r: 5, arr[l]: 3, arr[r]: 5, lmax: 4, rmax: 5, sum: 1, maxWaterSum: 7
+   * l: 4, r: 5, arr[l]: 4, arr[r]: 5, lmax: 4, rmax: 5, sum: 2, maxWaterSum: 9
+   */
   
-  // Naive Idea: leftMax Arr + rightMax Arr + water Arr 
+  // Naive Idea: leftMax Arr + rightMax Arr + water Arr (DP on wasterSum)
   // Time: O(N)
   // Space: O(3N)
   public int maxTrapped(int[] array) {
