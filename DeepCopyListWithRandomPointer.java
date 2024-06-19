99ac7bb6547ed2e1e59593ccb9b4615559cbee80
blue.sea <tldr.code@gmail.com>
2024-06-18 23:49:04 -0700

    Update DeepCopyListWithRandomPointer.java

--- DeepCopyListWithRandomPointer.java
+++ DeepCopyListWithRandomPointer.java
@@ -1,18 +1,17 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
+/**
+ * Definition for a Node.
+ * class Node {
+ *     int val;
+ *     Node next;
+ *     Node random;
+ *
+ *     public Node(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *         this.random = null;
+ *     }
+ * }
+ */
 public class DeepCopyListWithRandomPointer {
     public Node copyRandomList(Node head) {
         // corner case 
