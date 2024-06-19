4695510573e5fda8f0bdc32c2c5fe180c12ad11b
blue.sea <tldr.code@gmail.com>
2024-06-19 02:21:51 -0700

    Update MaxWaterTrapped3D.java.java

--- GraphDFS/MaxWaterTrapped3D.java
+++ GraphDFS/MaxWaterTrapped3D.java
@@ -1,4 +1,6 @@
-/** 
+package GraphDFS;
+
+/**
  * Given a non-negative integer 2D array representing the heights of bars in a matrix. 
  * Suppose each bar has length and width of 1. Find the largest amount of water that can be trapped in the matrix. 
  * The water can flow into a neighboring bar if the neighboring bar's height is smaller than the water's height. 
