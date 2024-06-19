821164ee7c72a75ce4fc24a3dd3d6990485fbe26
blue.sea <tldr.code@gmail.com>
2024-06-19 01:16:30 -0700

    Update MinimumSizeSubarraySum.java

--- SlidingWindow/MinimumSizeSubarraySum.java
+++ SlidingWindow/MinimumSizeSubarraySum.java
@@ -1,6 +1,5 @@
-package SlidingWindow;
-
 /**
+ * 209. Minimum Size Subarray Sum
  * Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray
  * whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
  *
@@ -16,7 +15,8 @@ package SlidingWindow;
  */
 class MinimumSizeSubarraySum {
     // Sliding Window: 同向双指针
-    // Time: O(n), Space: O(1)
+    // Time: O(n)
+    // Space: O(1)
     public int minSubArrayLen(int target, int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
