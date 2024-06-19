69648191ac2e5f8d8c4656501fb0e0dbf645d288
blue.sea <tldr.code@gmail.com>
2024-06-18 22:16:38 -0700

    Update FirstMissingPositive.java

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -6,23 +6,27 @@
  * Input: [7,8,9,11,12]  Output: 1
  */
 class FirstMissingPositive {
+    // Algo: Sorting 先排序，然后找到没有在正确位置上的 smallest missing num
+    // Time: O(n)
+    // Space: O(n) in place sorting
     public int firstMissingPositive(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            while (array[i] > 0 && array[i] <= array.length 
-                   && array[array[i] - 1] != array[i]) {
+            while (array[i] > 0 && array[i] <= array.length
+                    && array[array[i] - 1] != array[i]) {
                 // 满足在指定范围内、并且没有放在正确的位置上，才交换
                 // (e.g.) 3 should be placed at index 2
                 swap(array, i, array[i] - 1);
             }
         }
-        
+
         for (int i = 0; i < array.length; i++) {
             if (array[i] != i + 1) {
+                // 找到没有在正确位置上的 smallest missing num
                 return i + 1;
             }
         }
         return array.length + 1;
-  }
+    }
   
     private void swap(int[] array, int left, int right) {
         int tmp = array[left];
