b3dd49bceafd85cadfc853995b5c97b09f2e8d65
blue.sea <tldr.code@gmail.com>
2024-06-19 10:55:39 -0700

    Update DeepCopyUndirectedGraph.java

--- Graph/DeepCopyUndirectedGraph.java
+++ Graph/DeepCopyUndirectedGraph.java
@@ -44,18 +44,18 @@ class DeepCopyUndirectedGraph {
         for (GraphNode node : graph) {
             if (!map.containsKey(node)) {
                 map.put(node, new GraphNode(node.key));
-                DFS(node, map);
+                dfs(node, map);
             }
         }
         return new ArrayList<GraphNode>(map.values());
     }
 
-    private void DFS(GraphNode seed, Map<GraphNode, GraphNode> map) {
+    private void dfs(GraphNode seed, Map<GraphNode, GraphNode> map) {
         GraphNode copy = map.get(seed);
         for (GraphNode nei : seed.neighbors) {
             if (!map.containsKey(nei)) {
                 map.put(nei, new GraphNode(nei.key));
-                DFS(nei, map);
+                dfs(nei, map);
             }
             copy.neighbors.add(map.get(nei));
         }
