55987b55d44223a5ea82091882fe73fc3162e17e
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-24 13:00:02 -0700

    Update LongestValidParentheses.java

--- FreqMap/LongestValidParentheses.java
+++ FreqMap/LongestValidParentheses.java
@@ -2,16 +2,6 @@ package FreqMap;
 
 /**
  * 32. Longest Valid Parentheses
- * Hard
- * Topics
- * Companies
- * Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses
- * substring
- * .
- *
- *
- *
- * Example 1:
  *
  * Input: s = "(()"
  * Output: 2
@@ -26,7 +16,6 @@ package FreqMap;
  * Input: s = ""
  * Output: 0
  */
-
 public class LongestValidParentheses {
     // Method 1: Mono Stack store the index of (unmateched left-parentheses)
     // Time: O(n), Space: O(n)
