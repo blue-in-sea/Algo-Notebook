a099fa38b8f1b3f1e52a2b17f7760a6113fc6850
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-23 17:47:47 -0700

    Update RobotBoundedInCircle.java

--- OOP/RobotBoundedInCircle.java
+++ OOP/RobotBoundedInCircle.java
@@ -2,12 +2,7 @@
  * 1041. Robot Bounded In Circle
  * On an infinite plane, a robot initially stands at (0, 0) and faces north. Note that:
  *
- * The north direction is the positive direction of the y-axis.
- * The south direction is the negative direction of the y-axis.
- * The east direction is the positive direction of the x-axis.
- * The west direction is the negative direction of the x-axis.
  * The robot can receive one of three instructions:
- *
  * "G": go straight 1 unit.
  * "L": turn 90 degrees to the left (i.e., anti-clockwise direction).
  * "R": turn 90 degrees to the right (i.e., clockwise direction).
