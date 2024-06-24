7603c5494eced429e7e2e6350f003853963d5b05
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-23 17:47:24 -0700

    Update RobotBoundedInCircle.java

--- OOP/RobotBoundedInCircle.java
+++ OOP/RobotBoundedInCircle.java
@@ -1,3 +1,32 @@
+/**
+ * 1041. Robot Bounded In Circle
+ * On an infinite plane, a robot initially stands at (0, 0) and faces north. Note that:
+ *
+ * The north direction is the positive direction of the y-axis.
+ * The south direction is the negative direction of the y-axis.
+ * The east direction is the positive direction of the x-axis.
+ * The west direction is the negative direction of the x-axis.
+ * The robot can receive one of three instructions:
+ *
+ * "G": go straight 1 unit.
+ * "L": turn 90 degrees to the left (i.e., anti-clockwise direction).
+ * "R": turn 90 degrees to the right (i.e., clockwise direction).
+ * The robot performs the instructions given in order, and repeats them forever.
+ *
+ * Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+ *
+ * Input: instructions = "GGLLGG"
+ * Output: true
+ * Explanation: The robot is initially at (0, 0) facing the north direction.
+ * "G": move one step. Position: (0, 1). Direction: North.
+ * "G": move one step. Position: (0, 2). Direction: North.
+ * "L": turn 90 degrees anti-clockwise. Position: (0, 2). Direction: West.
+ * "L": turn 90 degrees anti-clockwise. Position: (0, 2). Direction: South.
+ * "G": move one step. Position: (0, 1). Direction: South.
+ * "G": move one step. Position: (0, 0). Direction: South.
+ * Repeating the instructions, the robot goes into the cycle: (0, 0) --> (0, 1) --> (0, 2) --> (0, 1) --> (0, 0).
+ * Based on that, we return true.
+ */
 class RobotBoundedInCircle {
     // Time:  O(N) where N is a number of instructions to parse.
     // Space: O(1) mathcal O(1) because the array directions contains only 4 elements.
