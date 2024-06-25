64f5422eb8beee46824654374c67c99dfeb39248
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-25 15:28:14 -0700

    Update MaximumSubarray.java

--- DP/MaximumSubarray.java
+++ DP/MaximumSubarray.java
@@ -8,18 +8,17 @@
  * {-2, -1, -3},      the largest subarray sum is -1
  */
 public class MaximumSubarray {
-  /** 
-   * Method 1: Dynamic Programming  
-   * Time: O(n) for linear scan, Space: O(1)
-   */
+  
+  // M[i] means the largest sum of subarray ending at index i
+  // M[i] = array[i]           if M[i-1] <= 0
+  //      = M[i-1] + array[i]  if M[i-1] > 0
+   
+  // Time: O(n) for linear scan, Space: O(1)
   public int largestSum(int[] array) {
     // Assume Array != null && length >= 1
     // The subarray must contains at leaste one elements
     int[] M = new int[array.length];
     M[0] = array[0];
-    // M[i] means the largest sum of subarray ending at index i
-    // M[i] = array[i]           if M[i-1] <= 0
-    //      = M[i-1] + array[i]  if M[i-1] > 0
     int res = M[0];
     for (int i = 1; i < array.length; i++) {
       M[i] = Math.max(M[i - 1] + array[i], array[i]); 
@@ -28,10 +27,8 @@ public class MaximumSubarray {
     return res;
   }
   
-  /** 
-   * DP: We can reduce the space consumption by recording the latest largest sum
-   * Time: O(1) for linear scan, Space: O(1)
-   */
+   // DP reduce the space consumption by ONLY store the latest largest sum
+   // Time: O(1) for linear scan, Space: O(1)
   public int largestSum(int[] array) {  
     int maxSum = array[0];
     int lastSum = array[0]; 
@@ -42,5 +39,4 @@ public class MaximumSubarray {
     }
     return maxSum;
   }
-  
 }
