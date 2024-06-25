008a085199cec45ea3a7bbd29988b13defc21e58
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-25 15:24:29 -0700

    Update MaximumSubarray.java

--- DP/MaximumSubarray.java
+++ DP/MaximumSubarray.java
@@ -4,11 +4,9 @@
  * Assumptions 
  * The given array is not null and has length of at least 1.
  *
- * Examples
  * {2, -1, 4, -2, 1}, the largest subarray sum is 2 + (-1) + 4 = 5
  * {-2, -1, -3},      the largest subarray sum is -1
  */
-
 public class MaximumSubarray {
   /** 
    * Method 1: Dynamic Programming  
