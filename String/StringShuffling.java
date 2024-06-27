2cbc2e527390fb800b7a8b4204dc402f645388fd
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-26 18:40:55 -0700

    Update StringShuffling.java

--- String/StringShuffling.java
+++ String/StringShuffling.java
@@ -1,5 +1,19 @@
-package String;
-
+/**
+ * ReOrderArray (LaiCode 197)
+ * Given an array of elements, reorder it as follow:
+ *
+ * { N1, N2, N3, …, N2k } → { N1, Nk+1, N2, Nk+2, N3, Nk+3, … , Nk, N2k }
+ *
+ * { N1, N2, N3, …, N2k+1 } → { N1, Nk+1, N2, Nk+2, N3, Nk+3, … , Nk, N2k, N2k+1 }
+ *
+ * Try to do it in place.
+ *
+ * { 1, 2, 3, 4, 5, 6} → { 1, 4, 2, 5, 3, 6 }
+ *
+ * { 1, 2, 3, 4, 5, 6, 7, 8 } → { 1, 5, 2, 6, 3, 7, 4, 8 }
+ *
+ * { 1, 2, 3, 4, 5, 6, 7 } → { 1, 4, 2, 5, 3, 6, 7 }
+ */
 public class StringShuffling {
     public int[] reorder(int[] array) {
         // Assumption: array is not null
