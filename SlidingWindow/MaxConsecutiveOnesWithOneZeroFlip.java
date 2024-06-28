47b6ae4cf8b3e9b2590e1aa6d616afac5b0e750e
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-06-27 17:55:59 -0700

    Update MaxConsecutiveOnesWithOneZeroFlip.java

--- SlidingWindow/MaxConsecutiveOnesWithOneZeroFlip.java
+++ SlidingWindow/MaxConsecutiveOnesWithOneZeroFlip.java
@@ -1,6 +1,7 @@
 /**
  * 487. Max Consecutive Ones II
- * 
+ * Given a binary array nums, return the maximum number of consecutive 1's in the array if you can flip at most one 0.
+ *
  * Input: nums = [1,0,1,1,0]
  * Output: 4
  * Explanation: 
@@ -16,7 +17,13 @@
  * The max number of consecutive ones is 4.
  */
 class MaxConsecutiveOnesWithOneZeroFlip {
-        
+    /**
+     * Sliding Window: 同向双指针 
+     * Finding the max number of consecutive 1's in the array with at most 1 zero flip
+     *
+     * Time: O(n)
+     * Space: O(1)
+     */     
     public int findMaxConsecutiveOnes(int[] nums) {
         int max = 0;
         int l = 0;
