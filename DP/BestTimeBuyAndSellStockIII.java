48c03f6f3c780d451f8ffe67b57f68111fd564d6
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-03 13:26:39 -0700

    Update BestTimeBuyAndSellStockIII.java

--- DP/BestTimeBuyAndSellStockIII.java
+++ DP/BestTimeBuyAndSellStockIII.java
@@ -28,12 +28,11 @@ public class BestTimeBuyAndSellStockIII {
      *          The minimal cost to acquire a stock would be the minimal price value that we have seen so far at each step.
      *
      * t1_profit: the maximal profit of selling the stock in transaction #1.
-     *            Actually, at the end of the iteration, this value would be the answer for the first problem in the series,
-     *            i.e. Best Time to Buy and Sell Stock.
+     *            Actually, at the end of the iteration, this value would be the answer for the first problem in the series (BestTimeBuyAndSellStock.java)
      *
      * t2_cost: the minimal cost of buying the stock in transaction #2, while taking into account the profit gained from the previous transaction #1.
      *          One can consider this as the cost of reinvestment (Similar with t1_cost)
-     *          we try to find the lowest price so far, which in addition would be partially compensated by the profits gained from the first transaction.
+     *          we try to find the lowest price so far, which in addition would be partially compensated by the profits gained from the first transaction ** 
      *
      * t2_profit: the maximal profit of selling the stock in transaction #2.
      *            With the help of t2_cost as we prepared so far, we would find out the maximal profits with at most two transactions at each step.
