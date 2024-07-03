f50c5927ec6ae47398504db2f818ff98451765f7
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-03 16:05:50 -0700

    Update ValidPalindrome.java

--- String/ValidPalindrome.java
+++ String/ValidPalindrome.java
@@ -1,4 +1,28 @@
+/**
+ * 125. Valid Palindrome
+ * 
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric 
+ * characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+ *
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ *
+ * Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.
+ *
+ * Input: s = " "
+ * Output: true
+ * Explanation: s is an empty string "" after removing non-alphanumeric characters.
+ * Since an empty string reads the same forward and backward, it is a palindrome.
+ */
 public class ValidPalindrome {
+    // Algo (相向双指针) 首位相向而行
+    // Time: O(n) 
+    // Space: O(1)
     public boolean isPalindrome(String s) {
         int n = s.length();
         int left = 0, right = n - 1;
