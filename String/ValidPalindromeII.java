14b4861982854cecd0d54de718113dd5199c6c6c
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-03 16:12:24 -0700

    Update ValidPalindromeII.java

--- String/ValidPalindromeII.java
+++ String/ValidPalindromeII.java
@@ -1,5 +1,23 @@
+/**
+ * 680. Valid Palindrome II 
+ *
+ * Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+ *
+ * Input: s = "aba"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "abca"
+ * Output: true
+ * Explanation: You could delete the character 'c'.
+ *
+ * Input: s = "abc"
+ * Output: false
+ */
 public class ValidPalindromeII {
-    // Time: O(n), Space: O(1)
+    // Algo (相向双指针) 首位相向而行
+    // Time: O(n)
+    // Space: O(1)
     public boolean validPalindrome(String s) {
         // assume s is null and s has len equal 0 return true
         int l = 0, r = s.length() - 1;
