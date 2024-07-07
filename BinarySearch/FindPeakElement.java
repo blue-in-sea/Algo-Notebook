3259a5e8bbce6f9f9f0c3a643389c1bbd22bf028
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-06 20:57:39 -0700

    Update FindPeakElement.java

--- BinarySearch/FindPeakElement.java
+++ BinarySearch/FindPeakElement.java
@@ -1,3 +1,16 @@
+/**
+ * 162. Find Peak Element
+ * A peak element is an element that is strictly greater than its neighbors.
+ * 
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index number 2.
+ * Example 2:
+ *
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 5
+ * Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+ */
 public class FindPeakElement {
     // Binary Search 
     // Time: O(logn)
