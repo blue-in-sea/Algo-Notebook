cd59612ace3c791af89812bbb762a070466924b8
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-09 11:22:52 -0700

    Create 3Sum.java

---
+++ TwoSum/3Sum.java
@@ -0,0 +1,51 @@
+/**
+ * 15. 3Sum
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ *
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ * Explanation: 
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ * The distinct triplets are [-1,0,1] and [-1,-1,2].
+ * Notice that the order of the output and the order of the triplets does not matter.
+ *
+ * Input: nums = [0,1,1]
+ * Output: []
+ * Explanation: The only possible triplet does not sum up to 0.
+ *
+ * Input: nums = [0,0,0]
+ * Output: [[0,0,0]]
+ * Explanation: The only possible triplet sums up to 0.
+ */
+class 3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            // our goal is to find the index i, j, k such that 
+            // nums[i] + nums[j] - nums[k] = 0
+            // To make sure there is no duplicate triples 
+            // we ignore all duplicate possiable i
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int tmp = nums[j] + nums[k];
+                if (tmp + nums[i] == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;  // dedup j
+                    while (j < k && nums[k] == nums[k + 1]) k--;  // dedup k
+                } else if (tmp + nums[i] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+}
