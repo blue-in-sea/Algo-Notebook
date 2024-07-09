0f5cb6e56ed6c6dc29d1169ab8bfec413992fd8f
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-09 14:59:19 -0700

    Update 3SumClosest.java

--- TwoSum/3SumClosest.java
+++ TwoSum/3SumClosest.java
@@ -1,7 +1,20 @@
+/**
+ * 16. 3Sum Closest
+ * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+ * Return the sum of the three integers.
+ *
+ * Input: nums = [-1,2,1,-4], target = 1
+ * Output: 2
+ * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ *
+ * Input: nums = [0,0,0], target = 1
+ * Output: 0
+ * Explanation: The
+ */
 public class 3SumClosest {
-    // essence: search in sorted matrix
-    // time: O(nlogn) + O(n^2) = O(n^2)
-    // space: O(1) since two pointers
+    // Similar: search in sorted matrix
+    // Time: O(n^2) for O(nlogn) for sorting, O(n^2) for finding the pairs 
+    // space: O(1) since in place
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int res = nums[0] + nums[1] + nums[2];
