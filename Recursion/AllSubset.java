0dfac02a2e23e038c2565f56ec08ebc58ffe1f9b
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-10 14:27:42 -0700

    Update AllSubset.java

--- Recursion/AllSubset.java
+++ Recursion/AllSubset.java
@@ -6,6 +6,18 @@
  * Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
  */
 class AllSubset {
+    /**
+     * Algo: DFS
+     * 1. How many levels in the recursion tree?
+     *    N level (if n = input.length).
+     *    For each level, it makes the decision as to whether to put this element into the path set or not.
+     * 2. How many different states should we put on each node?
+     *    Two.
+     *    Each case considers select (or not).
+     *
+     * Time: O(2^n)
+     * Space: O(n) where n stack calls; at the bottom of each level, the path is size n
+     */
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
@@ -18,7 +30,7 @@ class AllSubset {
             result.add(new ArrayList<>(path));
             return;
         }
-
+        
         path.add(nums[index]);
         dfs(result, path, nums, index + 1);
         path.remove(path.size() - 1);
