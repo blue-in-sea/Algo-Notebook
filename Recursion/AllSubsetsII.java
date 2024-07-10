42f31f68cd1b1330915df564756ff04fbf5d3b81
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-10 14:46:26 -0700

    Update AllSubsetsII.java

--- Recursion/AllSubsetsII.java
+++ Recursion/AllSubsetsII.java
@@ -6,6 +6,18 @@
  * Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
  */
 class AllSubsetsII {
+    /**
+     * Algo: DFS
+     * 1. How many levels in the recursion tree?
+     *    N level (if n = input.length).
+     *    For each level, it makes the decision as to whether to put this element into the path set or not.
+     * 2. How many different states should we put on each node?
+     *    Two.
+     *    Each case considers select (or not).
+     *
+     * Time: O(n * 2^n) where 2^n to generate all subsets and then copy n element into output list
+     * Space: O(n) where n stack calls; at the bottom of each level, the path is size n
+     */
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
