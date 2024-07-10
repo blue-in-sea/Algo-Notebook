8d6437dc9bd4d29942f880bbe68f5380b7c4f2ac
blue.sea <tldr.code@gmail.com>
2024-07-10 01:06:52 -0700

    Update RestoreIPAddress.java

--- Recursion/RestoreIPAddress.java
+++ Recursion/RestoreIPAddress.java
@@ -1,4 +1,4 @@
-class Solution {
+class RestoreIPAddresses {
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
         if (s == null || s.length() == 0) {
@@ -10,6 +10,7 @@ class Solution {
     }
 
     private void dfs(char[] ip, List<String> res, StringBuilder sb, int offset, int level) {
+        // base case
         if (level == 4) {
             if (sb.length() == ip.length + 4) {
                 res.add(sb.substring(0, sb.length() - 1));
@@ -17,23 +18,25 @@ class Solution {
             }
         }
 
+        // state 1: one char in front of the current dot
         if (offset < ip.length) {
             sb.append(ip[offset]).append('.');
             dfs(ip, res, sb, offset + 1, level + 1);
             sb.delete(sb.length() - 2, sb.length());
         }
 
+        // state 2: two char in front of the current dot
         if (offset + 1 < ip.length) {
             char a = ip[offset];
             char b = ip[offset + 1];
             if (a != '0') {
                 sb.append(a).append(b).append('.');
                 dfs(ip, res, sb, offset + 2, level + 1);
+                sb.delete(sb.length() - 3, sb.length());
             }
-            sb.delete(sb.length() - 3, sb.length());
         }
 
-        // state 3: three char in front of each dot
+        // state 3: three char in front of the current dot
         if (offset + 2 < ip.length) {
             // valid ip range [0, 255]
             char a = ip[offset];
@@ -41,7 +44,7 @@ class Solution {
             char c = ip[offset + 2];
             if (a == '1' ||
                     a == '2' && b >= '0' && b <= '4' ||
-                    a == '2' && b >= '0' && b <= '4' && c >= '0' && c <= '5') {
+                    a == '2' && b >= '0' && b <= '5' && c >= '0' && c <= '5') {
                 sb.append(a).append(b).append(c).append('.');
                 dfs(ip, res, sb, offset + 3, level + 1);
                 sb.delete(sb.length() - 4, sb.length());
