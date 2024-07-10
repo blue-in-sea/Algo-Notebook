1058520662edbaba8594fb3e5f66c27089b45dc6
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-10 16:29:54 -0700

    Update Combination.java

--- Recursion/Combination.java
+++ Recursion/Combination.java
@@ -3,7 +3,7 @@
  * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n. You may return the answer in any order.
  *
  * Input: n = 4, k = 2
- * Output:
+ * Output: 
  * [
  *   [2,4],
  *   [3,4],
@@ -12,6 +12,8 @@
  *   [1,3],
  *   [1,4],
  * ]
+ * Explanation: There are 4 choose 2 = 6 total combinations.
+ * Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
  */
 class Combination {
     // Time: C(N, K) = (N−k)!k! / N!
