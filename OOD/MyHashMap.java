6d043cade66395d7e54334b9595f509cf6c97910
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-09 23:21:25 -0700

    Update MyHashMap.java

--- OOD/MyHashMap.java
+++ OOD/MyHashMap.java
@@ -1,3 +1,34 @@
+/**
+ * 706. Design HashMap
+ * Design a HashMap without using any built-in hash table libraries.
+ *
+ * MyHashMap() initializes the object with an empty map.
+ *   void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+ *   int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+ *   void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+ *
+ * ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+ * [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+ * Output
+ * [null, null, null, 1, -1, null, 1, null, -1]
+ *
+ * MyHashMap myHashMap = new MyHashMap();
+ * myHashMap.put(1, 1); // The map is now [[1,1]]
+ * myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+ * myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+ * myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+ * myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+ * myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+ * myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+ * myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+ */
+/**
+ * Time: O(N/K) for each of the methods
+ *     N is the number of all possible keys and
+ *     K is the number of predefined buckets in the hashmap, which is 2069 in our case
+ *
+ * Space: O(N + K) 
+ */
 class MyHashMap<K, V> {
     private class Entry<K, V> {
         K key;
@@ -38,7 +69,6 @@ class MyHashMap<K, V> {
         bucket.add(new Entry<Integer, Integer>(key, value));
     }
 
-    
     public int get(int key) {
         int index = getBucketIndex(key);
         // case 1: if key exit in the map, find the value
