e18bb2d287ad3c3bf5dfb1f0b4635c4a9818c75a
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-10 19:09:16 -0700

    Update MajorNumberI.java

--- FreqMap/MajorNumberI.java
+++ FreqMap/MajorNumberI.java
@@ -1,21 +1,5 @@
-package FreqMap;
-
 class MajorNumberI {
-    public int majorityElement(int[] nums) {
-        int majorityCount = nums.length / 2;
-        
-        for (int num : nums) {
-            int count = 0;
-            for (int elem : nums) {
-                if (elem == num) {
-                    count += 1;
-                }
-            }
-            if (count > majorityCount) return num;
-        }
-        
-        return -1;
-    }
+
     
     
     public int majorityElement(int[] nums) {
@@ -45,4 +29,24 @@ class MajorNumberI {
         }
         return map;
     } 
+
+    // ************************************************************************
+
+    // Method 2:
+
+    public int majorityElement(int[] nums) {
+        int majorityCount = nums.length / 2;
+        
+        for (int num : nums) {
+            int count = 0;
+            for (int elem : nums) {
+                if (elem == num) {
+                    count += 1;
+                }
+            }
+            if (count > majorityCount) return num;
+        }
+        
+        return -1;
+    }
 }
