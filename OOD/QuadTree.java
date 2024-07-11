92efbc64bd8c8a4399ec74661fa931f081869650
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-11 16:09:32 -0700

    Update QuadTree.java

--- OOD/QuadTree.java
+++ OOD/QuadTree.java
@@ -95,4 +95,46 @@ class QuadTree {
         return true;
     }
 
+    // ******************************************************************************
+    /**
+     * Optimized: Recursion on 4 sub-matrics !!
+     * 1. If length is one, return a new leaf node with value equal to the cell value at (x1, y1)
+     * 2. Otherwise,
+     *   a. Top-Left     (x1, y1)
+     *   b. Top-Right    (x1, y1 + length / 2)
+     *   c. Bottom-Left  (x1 + length / 2, y1)
+     *   d. Bottom-Right (x1 + length / 2, y1 + length / 2)
+     * 3. If all the four nodes returned by the above recursive calls are leaf nodes with the same value.
+     *    Then return a new leaf node with the same value.
+     * 4. Otherwise, return a non-leaf node with any value having child pointers pointing to the four above-returned nodes.
+     */
+    // Time: O(n^2) All the cells in the matrix will be iterated only once
+    // Space: O(logN) for stack calls
+    public Node construct(int[][] grid) {
+        return dfs(grid, 0, 0, grid.length);
+    }
+
+    private Node dfs(int[][] grid, int x1, int y1, int length) {
+        // Return a leaf node if the matrix size is one.
+        if (length == 1) {
+            return new Node(grid[x1][y1] == 1, true);
+        }
+
+        // Recursive calls to the four sub-matrices.
+        Node topLeft = dfs(grid, x1, y1, length / 2);
+        Node topRight = dfs(grid, x1, y1 + length / 2, length / 2);
+        Node bottomLeft = dfs(grid, x1 + length / 2, y1, length / 2);
+        Node bottomRight = dfs(grid, x1 + length / 2, y1 + length / 2, length / 2);
+
+        // If the four returned nodes are leaf and have the same values
+        // Return a leaf node with the same value.
+        if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf
+                && topLeft.val == topRight.val && topRight.val == bottomLeft.val
+                && bottomLeft.val == bottomRight.val) {
+            return new Node(topLeft.val, true);
+        }
+
+        // If the four nodes aren't identical, return a non-leaf node with corresponding child pointers.
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
 }
