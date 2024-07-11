9a1e9936900c5cd6b8fe923f7a4566646c06706f
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-10 17:05:57 -0700

    Update AllPermutation.java

--- Recursion/AllPermutation.java
+++ Recursion/AllPermutation.java
@@ -1,4 +1,16 @@
+/**
+ * 46. Permutations (No Dup)
+ * Given an array of unique integers, return all the possible permutations. You can return the answer in any order.
+ *
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ */
 class AllPermutation {
+    // Time: O(n * n!) where n! to find all permutations and then copy n element into output list
+    // Space: O(n) where n stack calls; at the bottom of each level, the path is size n
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         // in-place: so no need extra array to store each path
