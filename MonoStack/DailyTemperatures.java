54de3b7e9f66ec3d2189a0b774a75dfd91be5cc4
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-11 12:06:41 -0700

    Update DailyTemperatures.java

--- MonoStack/DailyTemperatures.java
+++ MonoStack/DailyTemperatures.java
@@ -1,5 +1,3 @@
-package MonoStack;
-
 /**
  * Given an array of integers temperatures represents the daily temperatures, return an array answer such that
  * answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
