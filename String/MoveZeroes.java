5c2cb1c4bfa628c25d2233290b75034c4c7cbba8
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-12 10:17:55 -0700

    Update MoveZeroes.java

--- String/MoveZeroes.java
+++ String/MoveZeroes.java
@@ -1,5 +1,27 @@
+/**
+ * Move All 0’s to the End
+ *
+ * index   0  1  2  3  4  5  6  7  8  9  10
+ * input   1  9  8  4  0  0  2  7  0  6  0
+ * return  1  9  8  4  2  7  6  0  0  0  0
+ *                              s
+ *                                       f
+ */
+/**
+ * 快慢同相双指针
+ * slow: all elements to the left side of slow (excluding slow) are non-zero
+ * fast: the current index of linear scan
+ *
+ * Initialize: s = 0, f = 0
+ * For each step
+ * case 1	a[f] == 0, then f++                                 (f and s index-gap is # of consecutive zeros)
+ * case 2 	a[f] != 0, then a[s] = a[f], s++, f++               (for non-zero element, replace with itself, or pushed forward)
+ *
+ * Post-processing fill 0s into rest of the array
+ * (if we use swap, we do not need post processing, since we do not kill the zeros)
+ */
 public class MoveZeroes {
-    // Two pointers: slow & fast
+    // Time: O(n), Space: O(1)
     public void moveZeroes(int[] nums) {
         // corner case
         if (nums == null || nums.length <= 1) return;
@@ -10,6 +32,7 @@ public class MoveZeroes {
             if (nums[f] != 0) {
                 nums[s] = nums[f];
                 s++;
+                // or swap(array, fast, slow++);
             }
 
             // case 2: nums[f] == 0, skip, f++
