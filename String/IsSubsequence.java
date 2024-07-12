c2b215889c6456ea8c50f47a712fa816a3ff074b
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-11 22:05:25 -0700

    Update IsSubsequence.java

--- String/IsSubsequence.java
+++ String/IsSubsequence.java
@@ -1,6 +1,36 @@
+/**
+ * 392. Is Subsequence
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ *
+ * A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) 
+ * of the characters without disturbing the relative positions of the remaining characters. 
+ * (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ *
+ * Input: s = "abc", t = "ahbgdc"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "axc", t = "ahbgdc"
+ * Output: false
+ */
 public class IsSubsequence {
     // check if s is subsequence of t
     // Time: O(n), Space: O(n) where n is the length of t-srting
+
+    // for-loop
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        int i = 0;
+        for (int j = 0; j < t.length(); j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                if (i == s.length()) return true;
+            }
+        }
+        return false;
+    }
+
+    // while-loop
     public boolean isSubsequence(String s, String t) {
         // corner case: empty string is subsequence of any string 
         if (s.length() == 0) return true;
@@ -11,18 +41,6 @@ public class IsSubsequence {
                 if (i == s.length()) return true;
             }
             j++;
-        }
-        return false;
-    }
-    
-    public boolean isSubsequence(String s, String t) {
-        if (s.length() == 0) return true;
-        int i = 0;
-        for (int j = 0; j < t.length(); j++) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
-                if (i == s.length()) return true;
-            }
         }
         return false;
     }
