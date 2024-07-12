f7564f9430532963f7cdb011d4b6b6f1d37f3466
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-12 09:56:55 -0700

    Create ArrayDuplicationII.java

---
+++ String/ArrayDuplicationII.java
@@ -0,0 +1,29 @@
+// Array Duplication II - Keep two elements for each dup in a sorted array
+// {1, 2, 2, 3, 3, 3} → {1, 2, 2, 3, 3}
+/**
+ * Solution: [0 … slow - 1] is the partial result to return, Fast pointer used to discover new element
+ * slow: all elements to the left side of slow (excluding slow) are results that are processed
+ * fast: the current index being processed 
+ * (all elements to the right side of the fast pointer has not been processed)
+ *
+ * Initialize: s = f = 2
+ * For each step
+ * case 1	a[f] == a[s-2], f++			                       (found the repeat element, skip)
+ * case 2 a[f] != a[s-2], then a[s] = a[f], s++, f++	   (replace itself or different element, then move)
+ */
+public class ArrayDuplicationII {
+    // Time: O(n), Space O(1)
+    public int[] dedup(int[] array) {
+        // assume array is not null
+        if (array.length <= 2) return array;
+
+        int slow = 2;
+        for (int fast = 2; fast < array.length; fast++) {
+            if (array[fast] == array[slow - 2]) {
+                continue;
+            }
+            array[slow++] = array[fast];
+        }
+        return Arrays.copyOf(array, slow);
+    }
+}
