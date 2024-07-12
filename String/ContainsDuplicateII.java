21cc9b33992736227fdc2f1ff8ca539fc2273bf1
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-12 10:22:37 -0700

    Update ContainsDuplicateII.java

--- String/ContainsDuplicateII.java
+++ String/ContainsDuplicateII.java
@@ -3,7 +3,6 @@
  * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array 
  * such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
  */
-
 public class ContainsDuplicateII {
   /**
    * Method 1: Naive Linear Search
