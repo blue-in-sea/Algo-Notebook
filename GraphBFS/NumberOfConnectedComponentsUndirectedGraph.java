04280789e0a0e9e129a20385ff8c258a9e086ffe
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-11 19:27:45 -0700

    Update NumberOfConnectedComponentsUndirectedGraph.java

--- GraphBFS/NumberOfConnectedComponentsUndirectedGraph.java
+++ GraphBFS/NumberOfConnectedComponentsUndirectedGraph.java
@@ -1,11 +1,30 @@
+/**
+ * 323. Number of Connected Components in an Undirected Graph
+ * Input: A graph of n nodes with a list of edges,
+ * Output: the number of connected components in the graph
+ *
+ * Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+ *     0 - 1             3
+ *        |    and      |
+ *        2             4
+ * Output: 2
+ * 
+ * 
+ * Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+ *     0 - 1      3
+ *        |   /   |
+ *        2       4
+ * Output: 1
+ */
 class NumberOfConnectedComponentsUndirectedGraph {
-    // E = Number of edges, V = Number of vertices.
-  
+    // E = Number of edges, V = Number of vertices
     // Time: O(V + E) 
-    // * Building the adjacency list will take O(E) since we iterate over the list of edges once
-    // * Graph traversal we visited every vertex once; for each vertex, we vised all its edges 
-  
-    // Space: O(V) size of graph 
+    //       Building the adjacency list will take O(E) since we iterate over the list of edges once
+    //       Graph traversal we visited every vertex once; for each vertex, we vised all its edges 
+    // Space: O(V) 
+    //        Size of graph 
+
+    // BFS
     public int countComponents(int n, int[][] edges) {
         if (n <= 1) return n;
 
@@ -38,6 +57,34 @@ class NumberOfConnectedComponentsUndirectedGraph {
         }
     }
 
+    // ***********************************************************************************************
+    // DFS
+    public int countComponents(int n, int[][] edges) {
+        if (n <= 1) return n;
+
+        Map<Integer, List<Integer>> graph = buildGraph(n, edges);
+        Set<Integer> visited = new HashSet<>();
+
+        int component = 0;
+        for (int i = 0; i < n; i++) {
+            if (visited.add(i)) {
+                component++;
+                dfs(graph, visited, i);
+            }
+        }
+
+        return component++;
+    }
+
+    private void dfs(Map<Integer, List<Integer>> graph, Set<Integer> visited, int curr) {
+        for (int nei : graph.get(curr)) {
+            if (visited.add(nei)) {
+                dfs(graph, visited, nei);
+            }
+        }
+    }
+    
+    // ***********************************************************************************************
     // Build the undirected (unconnect) graph
     private Map<Integer, List<Integer>> buildGraph(int n, int[][] edges) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
