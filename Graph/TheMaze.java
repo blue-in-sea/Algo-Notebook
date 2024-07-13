e1ee7a3920dee177da1cad6578bdcdaa25102bbe
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-13 16:33:01 -0700

    Update TheMaze.java

--- Graph/TheMaze.java
+++ Graph/TheMaze.java
@@ -1,3 +1,15 @@
+/**
+ * 490. The Maze
+ * There is a ball in a maze with empty spaces (represented as 0) and walls (represented as 1). The ball can go through 
+ * the empty spaces by rolling up, down, left or right, but it won't stop rolling until hitting a wall. 
+ * 
+ * When the ball stops, it could choose the next direction.
+ *
+ * Given the m x n maze, the ball's start position and the destination, where start = [startrow, startcol] and destination = [destinationrow, destinationcol], return true if the ball can stop at the destination, otherwise return false.
+ * Input: maze = [[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], start = [0,4], destination = [4,4]
+ * Output: true
+ * Explanation: One possible way is : left -> down -> left -> down -> right -> down -> right.
+ */
 class TheMaze {
     // DFS
     // Time: O(m⋅n⋅(m+n)) for matrix n by m
