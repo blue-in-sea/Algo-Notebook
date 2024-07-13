b1197c51d0368e9c0436d948df56bce0190836f7
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-13 11:36:14 -0700

    Update SubarraySumEqualsK.java

--- DP/SubarraySumEqualsK.java
+++ DP/SubarraySumEqualsK.java
@@ -9,28 +9,53 @@
  * Output: 2 for [1,2] and [3]
  */
 public class SubarraySumEqualsK {
-  // Methdod 1: Sliding Window
-  // Find subarraySum(i, j) == target, increment the cnt
-  // Time: O(n^2), Space: O(1)
+  // Methdod 0: DP pre_sum + HashMap <K = pre_sum, V = Freq>
+  
+  // To find the subarraySum(i, j) =>                         
+  //    find the num[j] such that (pre_sum[j] - k) in the map 
+  
+  // Time: O(n), Space: O(n)
   public int numOfSubarraySumToK(int[] nums, int k) {
     if (nums == null || nums.length == 0) {
       return -1;
     }
 
-    int cnt = 0;
-    for (int i = 0; i < nums.length; i++) {
-      int sum = nums[i];
-      if (sum == k) cnt++;
+    int sum = 0, cnt = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
 
-      for (int j = i + 1; j < nums.length; j++) {
-        sum += nums[j];
-        if (sum == k) cnt++;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+
+      if (map.containsKey(sum - k)) {
+        cnt += map.get(sum - k);
       }
+      map.put(sum , map.getOrDefault(sum, 0) + 1);
     }
 
     return cnt;
   }
-  
+
+  // *********************************************************************
+
+  // Brutal Force 
+  // For each subarray(i, j) -> check if subarray sum equal k
+  // Time: O(n^3)
+  // Space: O(1)
+  public int subarraySum(int[] nums, int k) {
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int sum = 0;
+                for (int x = i; x <= j; x++) {
+                    sum += nums[x];
+                }
+                if (sum == k) cnt++;
+            }
+        }
+        return cnt;
+    }
+
   // Methdod 2: DP to store the pre_sum
   // DP[i] represents the pre_sum including a[i]
   // Time: O(n^2), Space: O(1)
@@ -57,26 +82,26 @@ public class SubarraySumEqualsK {
 
     return cnt;
   }
-  
-  // Methdod 3: DP pre_sum + HashMap <K = pre_sum, V = Freq>
-  // DP[i] represents the pre_sum including a[i]
-  // Time: O(n), Space: O(n)
+
+  // *********************************************************************
+
+  // Methdod 3: Sliding Window (not recommended)
+  // Find subarraySum(i, j) == target, increment the cnt
+  // Time: O(n^2), Space: O(1)
   public int numOfSubarraySumToK(int[] nums, int k) {
     if (nums == null || nums.length == 0) {
       return -1;
     }
 
-    int sum = 0, cnt = 0;
-    Map<Integer, Integer> map = new HashMap<>();
-    map.put(0, 1);
-
+    int cnt = 0;
     for (int i = 0; i < nums.length; i++) {
-      sum += nums[i];
+      int sum = nums[i];
+      if (sum == k) cnt++;
 
-      if (map.containsKey(sum - k)) {
-        cnt += map.get(sum - k);
+      for (int j = i + 1; j < nums.length; j++) {
+        sum += nums[j];
+        if (sum == k) cnt++;
       }
-      map.put(sum , map.getOrDefault(sum, 0) + 1);
     }
 
     return cnt;
