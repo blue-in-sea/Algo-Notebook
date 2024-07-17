69e9db6e909646a7133653070e5a11a69607acc9
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-17 12:47:47 -0700

    Update and rename ClosestInSortedArray.java to FindClosestInSortedArray.java

--- BinarySearch/FindClosestInSortedArray.java
+++ BinarySearch/FindClosestInSortedArray.java
@@ -11,8 +11,7 @@
  * A = {1, 4, 6}, T = 5, return 1 or 2
  * A = {1, 3, 3, 4}, T = 2, return 0 or 1 or 2
  */
-
-public class ClosestInSortedArray {
+public class FindClosestInSortedArray {
     public int closest(int[] array, int target) {
         if (array == null || array.length == 0) {
             return -1;
