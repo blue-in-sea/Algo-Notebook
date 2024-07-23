258723eca4d37dcd50a28086c154744a4ddc51ad
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-23 13:15:14 -0700

    Update SearchInSortedMatrixI.java

--- BinarySearch/SearchInSortedMatrixI.java
+++ BinarySearch/SearchInSortedMatrixI.java
@@ -12,30 +12,30 @@
  * target = 7, return {1, 2}
  * target = 6, return {-1, -1} to represent the target number does not exist in the matrix.
  */
-
 public class SearchInSortedMatrixI {
-  public int[] search(int[][] matrix, int target) {
-    int[] result = new int[] { -1, -1 };
-    if (matrix.length == 0 || matrix[0].length == 0) {
-      return result;
-    }
-    int rows = matrix.length;
-    int cols = matrix[0].length;
+    public int[] search(int[][] matrix, int target) {
+        int[] result = new int[] { -1, -1 };
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+      
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-    int left = 0;
-    int right = rows * cols - 1;
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      int row = mid / cols;  
-      int col = mid % cols;
-      if (matrix[row][col] == target) {
-        return new int[] { row, col };
-      } else if (matrix[row][col] < target) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+        int left = 0;
+        int right = rows * cols - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / cols;
+            int col = mid % cols;
+            if (matrix[row][col] == target) {
+                return new int[] { row, col };
+            } else if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
     }
-    return new int[] { -1, -1 };
-  }
 }
