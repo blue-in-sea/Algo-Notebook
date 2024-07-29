de791e205c8964d8fd1c7951d3102f931f8d69fa
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-07-29 11:33:58 -0700

    Update AllSubsetsOfSizeK.java

--- Recursion/AllSubsetsOfSizeK.java
+++ Recursion/AllSubsetsOfSizeK.java
@@ -12,23 +12,40 @@ public class AllSubsetsOfSizeK {
         return list;
     }
 
-    private void DFS(List<String> list, StringBuilder sb,
-                     int index, int k, char[] array) {
-        // base case: we have picked k elements, we add the subset
-        // to the result, and return
+    private void DFS(List<String> list, StringBuilder sb, int index, int k, char[] array) {
+        // base case: we have picked k elements, we add the subset to the result, and return
         if (sb.length() == k) {
             list.add(sb.toString());
             return;
         }
+        
         // base case: we've decided for all characters pick or not, we
         // reach the last index of the input array, we must return
         if (index == array.length) {
             return;
         }
+        
         // 1. pick the char at the index i
         DFS(list, sb.append(array[index]), index + 1, k, array);
         sb.deleteCharAt(sb.length() - 1);
         // 2. not pick the char at the index i
         DFS(list, sb, index + 1, k, array);
+    }
+
+    // version 2
+    private void dfs(List<String> res, StringBuilder sb, int index, int k, char[] array) {
+        // base case: we have picked k elements, we add the subset 
+        // to the result, and return 
+        if (sb.length() == k) {
+            res.add(sb.toString());
+            return;
+        }
+
+        for (int i = index; i < array.length; i++) {
+            if (i > index && array[i] == array[i - 1]) continue;
+            sb.append(array[i]);
+            dfs(res, sb, i + 1, k, array);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
