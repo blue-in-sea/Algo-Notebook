6155bc6a7ccb34c43b5e8d73ed52b38d383c865f
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-04 22:48:55 -0700

    Update AllSubsetsOfSizeK.java

--- Recursion/AllSubsetsOfSizeK.java
+++ Recursion/AllSubsetsOfSizeK.java
@@ -1,3 +1,18 @@
+/**
+ * LaiCode 641. All Subsets II of Size K
+ * Given a set of characters represented by a String, return a list containing all subsets of the characters whose
+ * size is K. Notice that each subset returned will be sorted for deduplication.
+ *
+ * There could be duplicate characters in the original set.
+ *
+ *
+ * Set = "abc", K = 2, all the subsets are [“ab”, “ac”, “bc”].
+ * Set = "abb", K = 2, all the subsets are [“ab”, “bb”].
+ * Set = "abab", K = 2, all the subsets are [“aa”, “ab”, “bb”].
+ * Set = "", K = 0, all the subsets are [""].
+ * Set = "", K = 1, all the subsets are [].
+ * Set = null, K = 0, all the subsets are [].
+ */
 public class AllSubsetsOfSizeK {
     // Time: O(2^k * k) 
     // Space: O(k) 
