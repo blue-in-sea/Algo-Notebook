d1535b114a971f9865367b4f61b9e469eaf43622
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-06 15:37:34 -0700

    Update MergeTwoSortedLists.java

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -1,14 +1,17 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * 21. Merge Two Sorted Lists
+ *
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ * Input: list1 = [], list2 = []
+ * Output: []
+ *
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
  */
 class MergeTwoSortedLists {
+    // Method 1: 谁小移谁
     // Time: O(n + m) where n is the size of one, m is the size of two
     // Space: O(1)
     public ListNode mergeTwoLists(ListNode one, ListNode two) {
@@ -26,7 +29,8 @@ class MergeTwoSortedLists {
             cur = cur.next;
 
         }
-
+        
+        // post-processing
         if (one != null) {
             cur.next = one;
         } else {
@@ -36,3 +40,13 @@ class MergeTwoSortedLists {
         return dummy.next;
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
