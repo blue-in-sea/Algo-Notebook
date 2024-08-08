525f4643b0937cbe4efa7109066b191e595aab55
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-07 22:06:24 -0700

    Update SortWith2Stacks.java

--- OOD/SortWith2Stacks.java
+++ OOD/SortWith2Stacks.java
@@ -1,5 +1,15 @@
-
-
+/**
+ * 280. Sort With 2 Stacks
+ * Given an array that is initially stored in one stack, sort it with one additional stacks (total 2 stacks).
+ *
+ * After sorting the original stack should contain the sorted integers and from top to bottom the integers are sorted
+ * in ascending order.
+ *
+ * The given stack is not null.
+ * There can be duplicated numbers in the give stack.
+ * 
+ * Requirements: No additional memory, time complexity = O(n ^ 2).
+ */
 public class SortWith2Stacks {
   // Time: O(n^2), No extra space created
   public void sort(LinkedList<Integer> s1) {
