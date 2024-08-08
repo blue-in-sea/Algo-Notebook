3092f0c2b59f0d2eab2520d117ac2c4314ea1e9a
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-07 23:28:40 -0700

    Update MaximumValuesOfSizeKSlidingWindows.java

--- MonoStack/MaximumValuesOfSizeKSlidingWindows.java
+++ MonoStack/MaximumValuesOfSizeKSlidingWindows.java
@@ -1,3 +1,16 @@
+/**
+ * 204. Maximum Values Of Size K Sliding Windows
+ * Given an integer array A and a sliding window of size K, find the maximum value of each window as it slides from 
+ * left to right.
+ *
+ * Assumptions
+ * The given array is not null and is not empty
+ *
+ * K >= 1, K <= A.length
+ *
+ * A = {1, 2, 3, 2, 4, 2, 1}, K = 3, the windows are {{1,2,3}, {2,3,2}, {3,2,4}, {2,4,2}, {4,2,1}},
+ * and the maximum values of each K-sized sliding window are [3, 3, 4, 4, 4]
+ */
 public class MaximumValuesOfSizeKSlidingWindows {
   // Method 1: Maintain a monolithic Deque 
   public List<Integer> maxWindows(int[] array, int k) {
