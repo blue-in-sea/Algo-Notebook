60d8079990d39348e8c9be33d3b78f9be998f9d2
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-07 22:57:13 -0700

    Update LargestNumber.java

--- Fun/LargestNumber.java
+++ Fun/LargestNumber.java
@@ -1,3 +1,15 @@
+/**
+ * 179. Largest Number
+ * Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+ * Since the result may be very large, so you need to return a string instead of an integer.
+ *
+ * Input: nums = [10,2]
+ * Output: "210"
+ * Example 2:
+ *
+ * Input: nums = [3,30,34,5,9]
+ * Output: "9534330"
+ */
 class LargestNumber {
     public String largestNumber(int[] nums) {
         String[] arr = new String[nums.length];
