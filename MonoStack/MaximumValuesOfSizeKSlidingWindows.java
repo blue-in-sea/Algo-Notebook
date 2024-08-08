6c8c76376a721ad2eb897e18ddca28356b857600
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-07 23:44:15 -0700

    Update MaximumValuesOfSizeKSlidingWindows.java

--- MonoStack/MaximumValuesOfSizeKSlidingWindows.java
+++ MonoStack/MaximumValuesOfSizeKSlidingWindows.java
@@ -51,8 +51,10 @@ public class MaximumValuesOfSizeKSlidingWindows {
     Comparator<Node> comparator = new Comparator<Node>() {
       public int compare(Node left, Node right) {
         if (right.val == left.val) {
+          // 如果相等，位置不变
           return left.pos - right.pos;    
         }
+        // 如果不相等，max first (单调非递减序列)
         return right.val - left.val;
       }  
     };
