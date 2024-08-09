1c98ae59aa7e8ab8e30b8963c99a8e5743375fb8
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-09 14:05:47 -0700

    Update MaximumValuesOfSizeKSlidingWindows.java

--- MonoStack/MaximumValuesOfSizeKSlidingWindows.java
+++ MonoStack/MaximumValuesOfSizeKSlidingWindows.java
@@ -80,3 +80,20 @@ public class MaximumValuesOfSizeKSlidingWindows {
     return res;
   }
 }
+/**
+ * Other examples
+ * Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * Output: [3,3,5,5,6,7]
+ * Explanation: 
+ * Window position                Max
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ *
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ */
