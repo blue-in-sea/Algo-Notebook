8b85bdc02a6a95bd38c7435948cc0f722dd5bc78
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-11 19:37:21 -0700

    Update CountSmaller.java

--- Sorting/CountSmaller.java
+++ Sorting/CountSmaller.java
@@ -1,6 +1,34 @@
+/**
+ * 1365. How Many Numbers Are Smaller Than the Current Number
+ * Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for
+ * each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+ *
+ * Return the answer in an array.
+ *
+ * Input: nums = [8,1,2,2,3]
+ * Output: [4,0,1,1,3]
+ *
+ * Input: nums = [6,5,4,8]
+ * Output: [2,1,0,3]
+ *
+ * Input: nums = [7,7,7,7]
+ * Output: [0,0,0,0]
+ *
+ *
+ * Constraints:
+ * 2 <= nums.length <= 500
+ * 0 <= nums[i] <= 100
+ */
 class CountSmaller {
-    // Time: O(n)
-    // Space: O(n) = count[101] + result[n] = O(1) + O(N)
+    /**
+     * Approach: counting
+     * We will create a count array to store the occurrences of each number in the input array. Then, we will iterate
+     * through this count array to accumulate the counts. Finally, we will use this information to determine the count of
+     * smaller numbers than the current number.
+     *
+     * Time: O(n)
+     * Space: O(N) where count[101] + result[n] = O(1) + O(N)
+     */
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] count = new int[101];
         int n = nums.length;
