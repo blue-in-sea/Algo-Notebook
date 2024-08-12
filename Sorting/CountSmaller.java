03c5403da70dbd2209c34b0911f30bf3ceab19fa
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-11 20:17:41 -0700

    Update CountSmaller.java

--- Sorting/CountSmaller.java
+++ Sorting/CountSmaller.java
@@ -52,4 +52,22 @@ class CountSmaller {
 
         return result;
     }
+
+    // ================================================
+    // Brutal Force O(n^2)
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+
+                result[i] = count;
+            }
+        }
+        return result;
+    }
 }
