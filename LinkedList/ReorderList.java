58b64fb93601be9f3ffdbb67e9e9f366c7096c48
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-12 18:22:54 -0700

    Update ReorderList.java

--- LinkedList/ReorderList.java
+++ LinkedList/ReorderList.java
@@ -1,12 +1,15 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * 86. Partition List
+ * Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes 
+ * greater than or equal to x. 
+ *
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ *
+ * Input: head = [1,4,3,2,5,2], x = 3
+ * Output: [1,2,2,4,3,5]
+ *
+ * Input: head = [2,1], x = 2
+ * Output: [1,2]
  */
 class Solution {
     // Time: O(n) There are three steps here. Each takes O(n)
@@ -74,3 +77,13 @@ class Solution {
         return slow;
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
