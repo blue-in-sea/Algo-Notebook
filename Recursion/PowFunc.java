2a2310b026189bf05bd51f3bccec1d458ed815a5
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-14 14:10:03 -0700

    Update PowFunc.java

--- Recursion/PowFunc.java
+++ Recursion/PowFunc.java
@@ -13,6 +13,27 @@
  * Explanation: 2-2 = 1/22 = 1/4 = 0.25
  */
 class PowFunc {
+    /**
+     * Brutal Force: pow(x, n) = x * pow(x, n - 1) where n<0.
+     *
+     * func pow(x, n):
+     *     if n == 0: return 1
+     *     if n < 0: return 1 / pow(x, -n)
+     *     return x * pow(x, n - 1)
+     *
+     *
+     * Optimization: Binary exponentiation
+     * (x^2)^(n/2) - n is even
+     * x * (x^2)^[(n-1)/2] - n is odd
+     *
+     * func pow(x, n):
+     *     if n == 0: return 1.0
+     *     if n < 0: return 1.0 / pow(x, -n)
+     *
+     *     if n is odd: return x * pow(x * x, (n - 1) / 2)
+     *     otherwise: return pow(x * x, n / 2)
+     */
+    // Time: O(logn), Space: O(logn) stack calls
     public double myPow(double a, int b) {
         if (a == 0 && b <= 0) {
             throw new ArithmeticException("illegal input");
