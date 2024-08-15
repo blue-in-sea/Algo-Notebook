8d1dd8821909839fa9b8801ba8776c219b558783
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-15 11:34:08 -0700

    Update ConstructAWordUsingDice.java

--- Recursion/ConstructAWordUsingDice.java
+++ Recursion/ConstructAWordUsingDice.java
@@ -1,4 +1,24 @@
 // https://leetcode.com/discuss/interview-question/267985/google-interview-construct-a-word-using-dice
+/**
+ * Given a word of length n and n six-sided dice with a character in each side. Find out if this word can be
+ * constructed by the set of given dice.
+ *
+ * Input:
+ * word = "hello"
+ * dice = [[a, l, c, d, e, f], [a, b, c, d, e, f], [a, b, c, h, e, f], [a, b, c, d, o, f], [a, b, c, l, e, f]]
+ * Output: true
+ * Explanation: dice[2][3] + dice[1][4] + dice[0][1] + dice[4][3] + dice[3][4]
+ *
+ * Input:
+ * word = "hello"
+ * dice = [[a, b, c, d, e, f], [a, b, c, d, e, f], [a, b, c, d, e, f], [a, b, c, d, e, f], [a, b, c, d, e, f]]
+ * Output: false
+ *
+ * Input:
+ * word = "aaaa"
+ * dice = [[a, a, a, a, a, a], [b, b, b, b, b, b], [a, b, c, d, e, f], [a, b, c, d, e, f]]
+ * Output: false
+ */
 class ConstructAWordUsingDice {
     public boolean combo(Character[][] dice, String word) {
         List<Set<Character>> words = new ArrayList<>();
