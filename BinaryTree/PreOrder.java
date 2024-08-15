cec5afdfcb21d5f637bfa5bfe03cb4d999447fe7
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-14 17:21:10 -0700

    Update PreOrder.java

--- BinaryTree/PreOrder.java
+++ BinaryTree/PreOrder.java
@@ -1,12 +1,17 @@
 /**
- * public class TreeNode {
- *   public int key;
- *   public TreeNode left;
- *   public TreeNode right;
- *   public TreeNode(int key) {
- *     this.key = key;
- *   }
- * }
+ * 655. Pre-order Traversal Of Binary Tree (recursive)
+ *
+ *         5
+ *
+ *       /    \
+ *
+ *     3        8
+ *
+ *   /   \        \
+ *
+ * 1      4        11
+ *
+ * Pre-order traversal is [5, 3, 1, 4, 8, 11]
  */
 public class PreOrder {
     public List<Integer> preOrder(TreeNode root) {
@@ -23,3 +28,13 @@ public class PreOrder {
         helper(root.right, res);
     }
 }
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
