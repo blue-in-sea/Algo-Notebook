838e34d7d80fa57a3125b7994c2ae1fa3ade0f9f
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-08-15 11:06:08 -0700

    Update RandomGenerator.java

--- OOD/RandomGenerator.java
+++ OOD/RandomGenerator.java
@@ -6,30 +6,30 @@
  * If possiblities are exhauted, return -1.
  */
 static class RandomGenerator {
-	int[] arr;
-	Random rand;
-	int cur = 0;
-	
-	public RandomGenerator(int n) {
-		arr = new int[n];
-		rand = new Random();
-		for(int i=0;i<arr.length;i++) {
-			arr[i] = i;
-			int next = rand.nextInt(i+1);
-			if(next != i) {
-        // swap(arr, i, next);
-				int tmp = arr[i];
-				arr[i] = arr[next];
-				arr[next] = tmp;
-			}
-		}
-	}
-	
-	public int generate() {
-		if(cur < arr.length)
-			return arr[cur++];
-		return -1;
-	}
+    int[] arr;
+    Random rand;
+    int cur = 0;
+
+    public RandomGenerator(int n) {
+        arr = new int[n];
+        rand = new Random();
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            int next = rand.nextInt(i+1);
+            if(next != i) {
+                // swap(arr, i, next);
+                int tmp = arr[i];
+                arr[i] = arr[next];
+                arr[next] = tmp;
+            }
+        }
+    }
+
+    public int generate() {
+        if(cur < arr.length)
+            return arr[cur++];
+        return -1;
+    }
 }
 
 public static void main(String[] args) {
