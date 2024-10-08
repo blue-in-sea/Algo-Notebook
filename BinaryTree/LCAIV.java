ca3a1db0703a0572d71ecbb134df6b62772c6b0f
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-07 23:23:28 -0700

    Update and rename LCAV.java to LCAIV.java

---
+++ BinaryTree/LCAIV.java
@@ -0,0 +1,57 @@
+/**
+ * 129. Lowest Common Ancestor IV
+ * Given K nodes in a binary tree, find their lowest common ancestor.
+ *
+ * K >= 2
+ * There is no parent pointer for the nodes in the binary tree
+ * The given K nodes are guaranteed to be in the binary tree
+ *
+ * Examples
+ *
+ *         5
+ *
+ *       /   \
+ *
+ *      9     12
+ *
+ *    /  \      \
+ *
+ *   2    3      14
+ *
+ * The lowest common ancestor of 2, 3, 14 is 5
+ * The lowest common ancestor of 2, 3, 9 is 9
+ */
+public class LCAIV {
+    public TreeNode lowestCommonAncestor(TreeNode root, List<TreeNode> nodes) {
+        // Assumption: K >=2, No parent pointers given, and K nodes are 
+        //             guaranteed to be in the binary tree
+        Set<TreeNode> set = new HashSet<>(nodes);
+        return LCA(root, set);
+    }
+    private TreeNode LCA(TreeNode root, Set<TreeNode> set) {
+        // 1. What should you do on current layer?
+        // base case: if root is null (or) root is one of the nodes in set 
+        //            we can ignore the later recursion 
+        if (root == null || set.contains(root)) {
+            return root;
+        }
+        // 2. What do you expect from left or right child
+        TreeNode l = LCA(root.left, set);
+        TreeNode r = LCA(root.right, set);
+        // 3. What should you report to the parent layer?
+        if (l != null && r != null) {
+            return root;
+        }
+        return l == null ? r : l;
+    }
+}
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
