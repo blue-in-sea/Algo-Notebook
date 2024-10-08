384b2618d370cb75d290f44d33a5c07a1a53a5e6
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-08 10:26:16 -0700

    Update InvertBinaryTreeUpsideDown.java

--- BinaryTree/InvertBinaryTreeUpsideDown.java
+++ BinaryTree/InvertBinaryTreeUpsideDown.java
@@ -1,12 +1,23 @@
 /**
- * public class TreeNode {
- *   public int key;
- *   public TreeNode left;
- *   public TreeNode right;
- *   public TreeNode(int key) {
- *     this.key = key;
- *   }
- * }
+ * 178. Reverse Binary Tree Upside Down
+ * Given a binary tree where all the right nodes are leaf nodes, flip it upside down and turn it into a tree with 
+ * left leaf nodes as the root.
+ *
+ * Examples
+ *
+ *         1
+ *       /    \
+ *     2        5
+ *   /   \
+ * 3      4
+ *
+ * is reversed to
+ *
+ *         3
+ *       /    \
+ *     2        4
+ *   /   \
+ * 1      5
  */
 public class InvertBinaryTreeUpsideDown {
   // Method 1: Recursion
@@ -29,20 +40,26 @@ public class InvertBinaryTreeUpsideDown {
   public TreeNode reverse(TreeNode root) {
     TreeNode prev = null;
     TreeNode prevRight = null;
-
     while (root != null) {
       TreeNode next = root.left;
       TreeNode curRight = root.right;
-
-      root.right = prevRight;  // make new right-connection
-      root.left = prev;        // make new left-connection
-
-      prevRight = curRight;    // prevRight move one forward
-      prev = root;             // prev move one forward
-
-      root = next;             // root move one forward
+      root.right = prevRight;  
+      root.left = prev;       
+      prevRight = curRight;    
+      prev = root;            
+      root = next;             
     }
-
     return prev;
   }
 }
+
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
