8a7ca46b37df0bb8ec11fdf14f279daabb43ea2d
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-07 17:32:41 -0700

    Update 2SumWithIndices.java

--- TwoSum/2SumWithIndices.java
+++ TwoSum/2SumWithIndices.java
@@ -1,3 +1,14 @@
+/**
+ * 181. 2 Sum All Pair I （with indices)
+ * Find all pairs of elements in a given array that sum to the given target number. Return all the pairs of indices.
+ *
+ * Assumptions
+ * The given array is not null and has length of at least 2.
+ *
+ * Examples
+ * A = {1, 3, 2, 4}, target = 5, return [[0, 3], [1, 2]]
+ * A = {1, 2, 2, 4}, target = 6, return [[1, 3], [2, 3]]
+ */
 class 2SumWithIndices {
     // Algo: HashMap <Key: num, Value: Index>
     // for i, for j such that nums[i] + nums[j] == target
