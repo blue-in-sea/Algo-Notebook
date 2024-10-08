f7a0cb419370106a2e2991526b46c2bcd919a69d
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-08 13:17:55 -0700

    Update DeleteNodeInBinarySearchTree.java

--- BinarySearchTree/DeleteNodeInBinarySearchTree.java
+++ BinarySearchTree/DeleteNodeInBinarySearchTree.java
@@ -1,4 +1,4 @@
-package BinarySearchTree; /**
+/**
  * 450. Delete Node in a BST
  *
  * Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
