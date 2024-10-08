3632977756ca5c8aadc6c1ae2fbdcb44f94d8c26
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-08 10:19:43 -0700

    Update DeepCopyListWithRandomPointer.java

--- LinkedList/DeepCopyListWithRandomPointer.java
+++ LinkedList/DeepCopyListWithRandomPointer.java
@@ -1,6 +1,41 @@
 /**
- * 138. Copy List with Random Pointer
- *
+ * Deep Copy Linked List With Random Pointer
+ * Each of the nodes in the linked list has another pointer pointing to a random node in the list or null. 
+ * Make a deep copy of the original list.
+ */
+public class DeepCopyListWithRandomPointer {
+    // HashMap: <K: node, V: node_copy> where the new linked list will be chained on node_copy
+    // Time: O(n), Space: O(n)
+    
+    public Node copyRandomList(Node head) {
+        if (head == null) return head;
+
+        Node dummy = new Node(0);
+        Node curr = dummy;
+       
+        Map<Node, Node> map = new HashMap<>();
+
+        while (head != null) {
+            if (!map.containsKey(head)) {
+                map.put(head, new Node(head.val));
+            }
+            curr.next = map.get(head);
+
+            if (head.random != null) {
+                if (!map.containsKey(head.random)) {
+                    map.put(head.random, new Node(head.random.val));
+                }
+                curr.next.random = map.get(head.random);
+            }
+
+            head = head.next;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
+/**
  * class Node {
  *     int val;
  *     Node next;
@@ -13,39 +48,3 @@
  *     }
  * }
  */
-public class DeepCopyListWithRandomPointer {
-    // HashMap: <K: node, V: node_copy> where the new linked list will be chained on node_copy
-    // Time: O(n)
-    // Space: O(n)
-    public Node copyRandomList(Node head) {
-        // corner case 
-        if (head == null) return head;
-        // dummy node help to construct the deep copy
-        Node dummy = new Node(0);
-        Node curr = dummy;
-        // maping the one-to-one relation in the given hashmap
-        Map<Node, Node> map = new HashMap<>();
-
-        while (head != null) {
-            if (!map.containsKey(head)) {
-                map.put(head, new Node(head.val));
-            }
-            // connect the copied node on the copy list 
-            curr.next = map.get(head);
-
-            // copy the random node if necessary
-            if (head.random != null) {
-                if (!map.containsKey(head.random)) {
-                    map.put(head.random, new Node(head.random.val));
-                }
-                // connect the copied node on the copy list 
-                curr.next.random = map.get(head.random);
-            }
-
-            head = head.next;
-            curr = curr.next;
-        }
-
-        return dummy.next;
-    }
-}
