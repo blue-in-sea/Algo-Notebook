76db0a58107ad8387fbdaff2a82544639ead7f8c
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-08 18:40:50 -0700

    Update ReconstructBinarySearchTreeWithPostorderTraversal.java

--- BinarySearchTree/ReconstructBinarySearchTreeWithPostorderTraversal.java
+++ BinarySearchTree/ReconstructBinarySearchTreeWithPostorderTraversal.java
@@ -1,22 +1,22 @@
 /**
  * 211. Reconstruct Binary Search Tree With Postorder Traversal
  * Given the postorder traversal sequence of a binary search tree, reconstruct the original tree.
+ *
  * postorder traversal = {1, 4, 3, 11, 8, 5}
  * the corresponding binary search tree is
  *         5
- *
  *       /    \
- *
  *     3        8
- *
  *   /   \        \
- *
  * 1      4        11
  * 
  * preorder: [5, 3, 1, 4, 8, 11]
  * inorder: [ 1, 3, 4, 5, 8, 11]
  */
 public class ReconstructBinarySearchTreeWithPostorderTraversal {
+    // the last element in the post order sequence has to be the root !!
+    // the root has to be the 'min' value at the current level !!
+    
     public TreeNode reconstruct(int[] post) {
         // Assume the given sequence is not null,
         // there is no duplicate in the binary search tree
@@ -45,8 +45,6 @@ public class ReconstructBinarySearchTreeWithPostorderTraversal {
         return root;
     }
 }
-
-
 /**
  * public class TreeNode {
  *   public int key;
