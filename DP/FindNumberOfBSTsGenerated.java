ecd9ab39b3f940198f8b1be61a36b4126de9c413
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-10 15:39:33 -0700

    Update FindNumberOfBSTsGenerated.java

--- DP/FindNumberOfBSTsGenerated.java
+++ DP/FindNumberOfBSTsGenerated.java
@@ -1,5 +1,14 @@
-package DP;
-
+/**
+ * 96. Unique Binary Search Trees (LeetCode)
+ * Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes
+ * of unique values from 1 to n.
+ *
+ * Input: n = 3
+ * Output: 5
+ *
+ * Input: n = 1
+ * Output: 1
+ */
 public class FindNumberOfBSTsGenerated {
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
