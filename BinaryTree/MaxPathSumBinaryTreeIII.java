c97f272a2ccf102ef64e4dd48469a997467317ac
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-10 15:26:08 -0700

    Update MaxPathSumBinaryTreeIII.java

--- BinaryTree/MaxPathSumBinaryTreeIII.java
+++ BinaryTree/MaxPathSumBinaryTreeIII.java
@@ -1,5 +1,6 @@
 /**
  * 140. Maximum Path Sum Binary Tree III (two node can be same node, and there only can be 1 path from Root to Leaf)
+ *
  * Given a binary tree in which each node contains an integer number. Find the maximum possible sub-path sum(both the 
  * starting and ending node of the subpath should be on the same path from root to one of the leaf nodes, and the 
  * sub-path is allowed to contain only one node).
@@ -21,16 +22,36 @@ public class MaxPathSumBinaryTreeIII {
     // Mathod 1: recursive
     // Time: O(H) where O(log N) for average case, O(N) worst case
     // Space: O(H) 
-    public TreeNode insert(TreeNode root, int key) {
+    public boolean exist(TreeNode root, int sum) {
         if (root == null) {
-            return new TreeNode(key);
+            return false;
         }
-        if (key < root.key) {
-            root.left = insert(root.left, key);
-        } else if (key > root.key) {
-            root.right = insert(root.right, key);
+
+        List<TreeNode> path = new ArrayList<>();
+        return dfs(root, path, sum);
+    }
+
+    private boolean dfs(TreeNode root, List<TreeNode> path, int sum) {
+        path.add(root);
+
+        int tmp = 0;
+        for (int i = path.size() - 1; i >= 0; i--) {
+            tmp += path.get(i).key;
+            if (tmp == sum) {
+                return true;
+            }
         }
-        return root;
+
+        if (root.left != null && dfs(root.left, path, sum)) {
+            return true;
+        }
+
+        if (root.right != null && dfs(root.right, path, sum)) {
+            return true;
+        }
+
+        path.remove(path.size() - 1);
+        return false;
     }
 }
 /**
