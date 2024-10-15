a3e54888816279136cca362db60ff5da5bb8232d
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-15 12:22:47 +0800

    Update KeepDistanceForIdenticalElements.java

--- Recursion/KeepDistanceForIdenticalElements.java
+++ Recursion/KeepDistanceForIdenticalElements.java
@@ -1,3 +1,13 @@
+/**
+ * 264. Keep Distance For Identical Elements
+ * Given an integer k, arrange the sequence of integers [1, 1, 2, 2, 3, 3, ...., k - 1, k - 1, k, k], such that the output integer array satisfy this condition:
+ * Between each two i's, they are exactly i integers (for example: between the two 1s, there is one number, between the two 2's there are two numbers).
+ *
+ * If there does not exist such sequence, return null.
+ * k is guaranteed to be > 0
+ *
+ * k = 3, The output = { 2, 3, 1, 2, 1, 3 }.
+ */
 public class Solution {
     // Method 3: k levels,
     // Time: O(k^k)
