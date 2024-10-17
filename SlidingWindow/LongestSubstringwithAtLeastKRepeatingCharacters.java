43dd99c813ee8c4b71ea45ef0854f897fbc6ccfc
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-17 09:42:12 +0800

    Update LongestSubstringwithAtLeastKRepeatingCharacters.java

--- SlidingWindow/LongestSubstringwithAtLeastKRepeatingCharacters.java
+++ SlidingWindow/LongestSubstringwithAtLeastKRepeatingCharacters.java
@@ -1,3 +1,17 @@
+/**
+ * 395. Longest Substring with At Least K Repeating Characters
+ * Given a string s and an integer k, return the length of the longest substring of s such that 
+ * the frequency of each character in this substring is greater than or equal to k.
+ * if no such substring exists, return 0.
+ *
+ * Input: s = "aaabb", k = 3
+ * Output: 3
+ * Explanation: The longest substring is "aaa", as 'a' is repeated 3 times.
+ *
+ * Input: s = "ababbc", k = 2
+ * Output: 5
+ * Explanation: The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+ */
 class LongestSubstringwithAtLeastKRepeatingCharacters {
     // Method 1: Silding Window
     // TLE
