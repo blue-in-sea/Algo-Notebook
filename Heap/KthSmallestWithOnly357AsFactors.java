22b7a4ff29fef753c7fdbdbac7f6521c03c51663
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-17 15:09:10 +0800

    Update KthSmallestWithOnly357AsFactors.java

--- Heap/KthSmallestWithOnly357AsFactors.java
+++ Heap/KthSmallestWithOnly357AsFactors.java
@@ -1,3 +1,17 @@
+/**
+ * 193. Kth Smallest With Only 3, 5, 7 As Factors
+ * Find the Kth smallest number s such that s = 3 ^ x * 5 ^ y * 7 ^ z, x > 0 and y > 0 and z > 0,
+ * x, y, z are all integers.
+ *
+ * Assumptions
+ *
+ * K >= 1
+ *
+ * the smallest is 3 * 5 * 7 = 105
+ * the 2nd smallest is 3 ^ 2 * 5 * 7 = 315
+ * the 3rd smallest is 3 * 5 ^ 2 * 7 = 525
+ * the 5th smallest is 3 ^ 3 * 5 * 7 = 945
+ */
 public class KthSmallestWithOnly3,5,7AsFactors {
     // Method 1: Min Heap
     // Time: O(klogn), Space: O(k)
