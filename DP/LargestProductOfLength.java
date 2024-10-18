28503d608a42e6a11b6f80ebef81fdcffa07a4b7
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2024-10-18 11:47:00 +0800

    Create LargestProductOfLength.java

---
+++ DP/LargestProductOfLength.java
@@ -0,0 +1,12 @@
+/**
+ * 191. Largest Product Of Length
+ * Given a dictionary containing many words, find the largest product of two words’ lengths, such that the two words do not share any common characters.
+ *
+ * Assumptions
+ * The words only contains characters of 'a' to 'z'
+ * The dictionary is not null and does not contains null string, and has at least two strings
+ * If there is no such pair of words, just return 0
+ * Examples
+ *
+ * dictionary = [“abcde”, “abcd”, “ade”, “xy”], the largest product is 5 * 2 = 10 (by choosing “abcde” and “xy”)
+ */
