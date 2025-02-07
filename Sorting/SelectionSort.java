3bd5930d29c3f28de51e78d81128a94261a4b1c0
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-02-07 13:42:01 -0800

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,32 +1,29 @@
-/**
- * Given an array of integers, sort the array in ascending order and return it.
- * [5,2,3,1] -> [1,2,3,5]
- */
 public class SelectionSort {
     /**
-     * Selection Sort
-     * select min for each index by scanning through rest of the array
-     *
-     * for i [0 -> n)
-     *    for j [i -> n)
-     *       if (a[i] > a[j])
-     *           swap (i, j)
+     * Selection sort is a sorting algorithm that selects the smallest element from an unsorted list in each iteration
+     * and places that element at the beginning of the unsorted list.
      *
      * Time: O(n^2)
      * Space: O(1)
      */
-    public int[] selectionSort(int[] nums) {
-        if (nums == null || nums.length == 0) return nums;
+    public int[] selectionSort(int[] arr) {
+        if (arr == null || arr.length == 0) return arr;
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] < nums[i]) {
-                    swap(nums, i, j);
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
                 }
             }
+
+            // swap
+            swap(arr, minIdx, i);
         }
 
-        return nums;
+        return arr;
     }
 
     private void swap(int nums[], int a, int b) {
