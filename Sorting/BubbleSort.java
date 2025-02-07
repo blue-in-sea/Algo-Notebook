50237b0d304985c622a19bb5709a1f9cb2c952e6
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-02-07 12:34:51 -0800

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,39 +1,44 @@
+import java.util.*;
 /**
  * Given an array of integers, sort the array in ascending order and return it.
- * [5,2,3,1] -> [1,2,3,5]
+ * Time: O(n2)
+ * Space: O(1)
+ * https://www.programiz.com/dsa/bubble-sort
  */
-public class BubbleSort {
+class BubbleSort {
     /**
-     * Selection Sort
-     * compares two adjacent elements and swaps them until they are in the intended order.
-     *
-     * for i [0 -> n)
-     *    for j [0 -> n - 1)
-     *        if (a[j] > a[j + 1])
-     *            swap (j, j+1)
-     *
-     * Time: O(n^2)
-     * Space: O(1)
+     * bubbleSort(array)
+     *   for i <- 1 to sizeOfArray - 1
+     *     for j <- 1 to sizeOfArray - 1 - i
+     *       if leftElement > rightElement
+     *         swap leftElement and rightElement
+     * end bubbleSort
      */
-    public int[] sortArray(int[] nums) {
-        if (nums == null || nums.length == 0) return nums;
+    static void bubbleSort(int array[]) {
+        int size = array.length;
 
-        int n = nums.length;
+        // loop to access each array element
+        for (int i = 0; i < size - 1; i++)
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    swap(nums, j, j + 1);
+            // loop to compare array elements
+            for (int j = 0; j < size - i - 1; j++)
+
+                // compare two adjacent elements
+                // change > to < to sort in descending order
+                if (array[j] > array[j + 1]) {
+
+                    // swapping occurs if elements
+                    // are not in the intended order
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
-            }
-        }
-
-        return nums;
     }
 
-    private void swap(int nums[], int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+    public static void main(String args[]) {
+        int[] data = { -2, 45, 0, 11, -9 }; // expect [-9, -2, 0, 11, 45]
+        bubbleSort(data);
+        System.out.println("Sorted Array in Ascending Order:");
+        System.out.println(Arrays.toString(data));
     }
 }
