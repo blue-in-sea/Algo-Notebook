18deb7c4ea3bfc85f49ba8ea6f4ef00c11114bc3
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-02-07 13:52:54 -0800

    Update 2Sum.java

--- TwoSum/2Sum.java
+++ TwoSum/2Sum.java
@@ -33,4 +33,20 @@ public class 2Sum {
         }
         return false;      
     }
+
+    // Return index of the only pair 
+    // Map<K: ele, V: index>
+    // Time: O(n), Space: O(n)
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
 }
