3d730d9bd847e34c8954abce9b5be64ced227eb5
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-02-15 15:58:37 -0800

    Update LongestAscendingSubsequence.java

--- DP/LongestAscendingSubsequence.java
+++ DP/LongestAscendingSubsequence.java
@@ -52,6 +52,8 @@ public class LongestAscendingSubsequence {
     return res;
   }
 
+  // ****************************************************************************
+
   // Method 2: DP + Binary Search
   // Time: O(nlogn), Space: O(n)
   public int longest2(int[] array) {
@@ -87,7 +89,8 @@ public class LongestAscendingSubsequence {
     return res;
   }
 
-  // find the largest smaller ele than target
+  // Binary search to find the largest index j such that tbl[j] < target
+  // (Equivalent to find the largest smaller ele than target)
   private int find(int[] tbl, int left, int right, int target) {
     while (left + 1 < right) {
       int mid = left + (right - left) / 2;
@@ -108,3 +111,61 @@ public class LongestAscendingSubsequence {
     return 0;
   }
 }
+/**
+ * The algorithm uses a greedy approach combined with binary search to efficiently find the LIS.
+ * It maintains tbl[] where:
+ *
+ * tbl[i] stores the smallest ending value of all increasing subsequences of length i.
+ *
+ * By keeping track of the smallest possible ending values, we can ensure that future
+ * elements can extend existing subsequences optimally. (greedy)
+ *
+ * array = [10, 9, 2, 5, 3, 7, 101, 18]
+ *
+ * Initialize tbl and res:
+ * tbl = [0, 10, 0, 0, 0, 0, 0, 0, 0]  size = arr_len + 1, tbl[0] = 0
+ * res = 1
+ *
+ * Process each element:
+ *
+ * array[1] = 9:
+ * find returns 0 (since 9 < 10).
+ * Update tbl[1] = 9.
+ * tbl = [0, 9, 0, 0, 0, 0, 0, 0, 0]
+ *
+ * array[2] = 2:
+ * find returns 0 (since 2 < 9).
+ * Update tbl[1] = 2.
+ * tbl = [0, 2, 0, 0, 0, 0, 0, 0, 0]
+ *
+ * array[3] = 5:
+ * find returns 1 (since 2 < 5).
+ * Update tbl[2] = 5.
+ * tbl = [0, 2, 5, 0, 0, 0, 0, 0, 0]
+ * res = 2
+ *
+ * array[4] = 3:
+ * find returns 1 (since 2 < 3).
+ * Update tbl[2] = 3.
+ * tbl = [0, 2, 3, 0, 0, 0, 0, 0, 0]
+ *
+ * array[5] = 7:
+ * find returns 2 (since 3 < 7).
+ * Update tbl[3] = 7.
+ * tbl = [0, 2, 3, 7, 0, 0, 0, 0, 0]
+ * res = 3
+ * 
+ * array[6] = 101:
+ * find returns 3 (since 7 < 101).
+ * Update tbl[4] = 101.
+ * tbl = [0, 2, 3, 7, 101, 0, 0, 0, 0]
+ * res = 4
+ * 
+ * array[7] = 18:
+ * find returns 3 (since 7 < 18).
+ * Update tbl[4] = 18.
+ * tbl = [0, 2, 3, 7, 18, 0, 0, 0, 0]
+ *
+ * Final Result:
+ * The length of the LIS is res = 4.
+ */
