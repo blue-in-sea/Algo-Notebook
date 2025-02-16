54fe48595587ed5f39bceb5cc78874c50121939d
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-02-15 17:40:07 -0800

    Update BestTimeBuyAndSellStockIV.java

--- DP/BestTimeBuyAndSellStockIV.java
+++ DP/BestTimeBuyAndSellStockIV.java
@@ -16,6 +16,39 @@
  * Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
  */
 public class BestTimeBuyAndSellStockIV {
+
+    // Time: O(n * k)
+    // Space: O(k) better than 2nd approach 
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (k == 0 || n == 0) return 0;
+
+        // If k is large enough, the problem reduces to the "Best Time to Buy and Sell Stock II" problem.
+        if (k >= n / 2) {
+            int maxProfit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+        Arrays.fill(buy, Integer.MAX_VALUE);
+
+        for (int price : prices) {
+            for (int j = 1; j <= k; j++) {
+                buy[j] = Math.min(buy[j], price - sell[j - 1]);
+                sell[j] = Math.max(sell[j], price - buy[j]);
+            }
+        }
+
+        return sell[k];
+    }
+
+    // *************************************************************
     /**
      * DP (2 ways to think of it)
      * 1. a brute-force approach and reduce unnecessary calculations
