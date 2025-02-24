78ea01e4545f55dd791603b976b4422663b95c05
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-02-24 11:58:52 -0800

    Update HouseRobber.java

--- DP/HouseRobber.java
+++ DP/HouseRobber.java
@@ -1,4 +1,5 @@
 /**
+ * House Robber (max profit)
  * You are a professional robber planning to rob houses along a street. Each house has a certain amount
  * of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses
  * have security system connected and it will automatically contact the police if two adjacent houses
@@ -11,7 +12,6 @@
  * Input: nums = [1,2,3,1], Output: 4
  * Rob house 1 (money = 1) and then rob house 3 (money = 3). Total amount you can rob = 1 + 3 = 4.
  */
-
 public class HouseRobber {
     public int rob(int[] nums) {
 
