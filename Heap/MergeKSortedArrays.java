214bc804d8da5f12c37b443cccc95f0f4c3022b0
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-02-25 11:47:21 -0800

    Update MergeKSortedArrays.java

--- Heap/MergeKSortedArrays.java
+++ Heap/MergeKSortedArrays.java
@@ -1,27 +1,31 @@
 public class MergeKSortedArrays {
+  // Time: O(N logK) 
+  // Space: O(N + K) size of results + size of heap 
   public int[] merge(int[][] arrayOfArrays) {
     // assume arrayOfArrays is not null, and none of the array is null
+    
     PriorityQueue<Entry> minHeap = new PriorityQueue<Entry>(11, new MyComparator());
     int len = 0;
     int k = arrayOfArrays.length;
+    
+    // Step 1: Populate the heap with the first element of each array
     for (int i = 0; i < k; i++) {
       int[] array = arrayOfArrays[i];
       len += array.length;
       if (array.length != 0) {
-        // we use two index to record the position of each time
-        // x - the index of the array in the arrayOfArrays
-        // y - the index of the element in the array
         minHeap.offer(new Entry(i, 0, array[0]));
       }
     }
 
+    // Step 2: Initialize the result array
     int[] result = new int[len];
-    int cur = 0;   // current index in the return list
+    int cur = 0;
+
+    // Step 3: Merge the arrays using the min-heap
     while (!minHeap.isEmpty()) {
       Entry tmp = minHeap.poll();
-      result[cur++] = tmp.value;  // add tmp into list
+      result[cur++] = tmp.value;
       if (tmp.y + 1 < arrayOfArrays[tmp.x].length) {
-        // reuse the same Entry object but advanced index by 1 unit
         tmp.y++;
         tmp.value = arrayOfArrays[tmp.x][tmp.y];
         minHeap.offer(tmp);
