2c7d86bf207fae90fbe73baeb857d187bc3c98f1
blue.sea <44347786+blue-in-sea@users.noreply.github.com>
2025-03-12 11:21:02 -0700

    Update KthClosestPointToOrigin.java

--- Heap/KthClosestPointToOrigin.java
+++ Heap/KthClosestPointToOrigin.java
@@ -1,56 +1,47 @@
-package Heap;
-
 public class KthClosestPointToOrigin {
-  // BFS2: Shortest Path Problem
-  public List<Integer> closest(int[] a, int[] b, int[] c, int k) {
-    PriorityQueue<List<Integer>> minHeap = new PriorityQueue(2 * k, new Comparator<List<Integer>>() {
-      @Override
-      public int compare(List<Integer> o1, List<Integer> o2) {
-        long d1 = distance(o1, a, b , c);
-        long d2 = distance(o2, a, b , c);
+  
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<List<Integer>> minHeap = new PriorityQueue((o1, o2) -> distance(o1, a, b , c) - distance(o1, a, b , c));
+        Set<List<Integer>> visited = new HashSet<>();
+        List<Integer> cur = Arrays.asList(0, 0, 0);
 
-        return d1 < d2 ? -1 : 1;
-      }
-    });
+        visited.add(cur);
+        minHeap.offer(cur);
 
-    Set<List<Integer>> visited = new HashSet<>();
-    List<Integer> cur = Arrays.asList(0, 0, 0);
+        while (k > 0) {
+            cur = minHeap.poll();
 
-    visited.add(cur);
-    minHeap.offer(cur);  // BFS 2
-    while (k > 0) {
-      cur = minHeap.poll();
+            List<Integer> n = Arrays.asList(cur.get(0) + 1, cur.get(1), cur.get(2));
+            if (n.get(0) < a.length && visited.add(n)) {
+                minHeap.offer(n);
+            }
 
-      List<Integer> n = Arrays.asList(cur.get(0) + 1, cur.get(1), cur.get(2));
-      if (n.get(0) < a.length && visited.add(n)) {
-        minHeap.offer(n);
-      }
+            n = Arrays.asList(cur.get(0), cur.get(1) + 1, cur.get(2));
+            if (n.get(1) < b.length && visited.add(n)) {
+                minHeap.offer(n);
+            }
 
-      n = Arrays.asList(cur.get(0), cur.get(1) + 1, cur.get(2));
-      if (n.get(1) < b.length && visited.add(n)) {
-        minHeap.offer(n);
-      }
+            n = Arrays.asList(cur.get(0), cur.get(1), cur.get(2) + 1);
+            if (n.get(2) < c.length && visited.add(n)) {
+                minHeap.offer(n);
+            }
 
-      n = Arrays.asList(cur.get(0), cur.get(1), cur.get(2) + 1);
-      if (n.get(2) < c.length && visited.add(n)) {
-        minHeap.offer(n);
-      }
+            k--;
+        }
 
-      k--;
+        // at last, we replace the index with the actual value in a, b, c
+        cur.set(0, a[cur.get(0)]);
+        cur.set(1, b[cur.get(1)]);
+        cur.set(2, c[cur.get(2)]);
+        return cur;
     }
 
-    // at last, we replace the index with the actual value in a, b, c
-    cur.set(0, a[cur.get(0)]);
-    cur.set(1, b[cur.get(1)]);
-    cur.set(2, c[cur.get(2)]);
-    return cur;
-  }
-
-  private long distance(List<Integer> point, int[] a, int[] b, int[] c) {
-    long d = 0;
-    d += a[point.get(0)] * a[point.get(0)];
-    d += b[point.get(1)] * b[point.get(1)];
-    d += c[point.get(2)] * c[point.get(2)];
-    return d;
-  }
+    private long distance(List<Integer> point, int[] a, int[] b, int[] c) {
+        long d = 0;
+        d += a[point.get(0)] * a[point.get(0)];
+        d += b[point.get(1)] * b[point.get(1)];
+        d += c[point.get(2)] * c[point.get(2)];
+        return d;
+    }
+  
 }
